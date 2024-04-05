7a03c5a630c519f25f050520cc4dace3d2d72953
mattjet27 <11801997+Mattjet27@users.noreply.github.com>
2024-04-05 21:49:06 +0200

    Add StartAOFunctionGeneration method and basic Unit Test for AOFunctionGen.

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DAQmx/AnalogOutputFunctionGeneration.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DAQmx/AnalogOutputFunctionGeneration.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NationalInstruments.DAQmx;
 using NationalInstruments.SemiconductorTestLibrary.Common;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
 using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
+using static NationalInstruments.SemiconductorTestLibrary.Common.Utilities;
 using static NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DAQmx.Utilities;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DAQmx
@@ -17,7 +19,7 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DAQ
         /// Configures the analog output function generation for the task (also known as PureTune).
         /// </summary>
         /// <param name="tasksBundle">The <see cref="DAQmxTasksBundle"/> object.</param>
-        /// <param name="functionGenerationSettings">T</param>
+        /// <param name="functionGenerationSettings">The function generation settings.</param>
         /// <exception cref="DaqException">The underling driver session returned an error.</exception>
         public static void ConfigureAOFunctionGeneration(this DAQmxTasksBundle tasksBundle, AOFunctionGenerationSettings functionGenerationSettings)
         {
@@ -51,6 +53,59 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DAQ
                 }
             });
             tasksBundle.Commit();
+        }
+
+        /// <summary>
+        /// Transitions the task to the running state to begin the analog output function generation.
+        /// This method will wait for the appropriate amount of settling time required by the insturment.
+        /// </summary>
+        /// <param name="tasksBundle">The <see cref="DAQmxTasksBundle"/> object.</param>
+        /// <exception cref="DaqException">The underling driver session returned an error.</exception>
+        public static void StartAOFunctionGeneration(this DAQmxTasksBundle tasksBundle)
+        {
+            var settlingTimes = tasksBundle.DoAndReturnPerInstrumentPerChannelResults((taskInfo, sitePinInfo) =>
+            {
+                return taskInfo.GetAOFunctionGenerationSettlingTime(sitePinInfo.IndividualChannelString);
+            });
+            // Want to ensure that all tasks are started at the same time.
+            // This is important for the outputs to start as synchronized as possible.
+            tasksBundle.Start();
+            // We also want to ensure we wait only once for the max settling time reuqired.
+            PreciseWait(settlingTimes.Max().Max());
+        }
+
+        /// <summary>
+        /// This method calculates the time required after starting a AO FuncGen task for the PXIe-4467 and PXIe-4468 for the output signal to settle.
+        /// If the task contains a DAQ insturment that does not support AO FuncGen, such as the PXIe-6368, then this function will return an error.
+        /// </summary>
+        /// <param name="taskInfo">The <see cref="DAQmxTaskInformation"/> object.</param>
+        /// <param name="individualChannelString">The individual channel string</param>
+        /// <returns>The max settling time across all devices, given the current channel's frequency.</returns>
+        /// <exception cref="NIMixedSignalException"></exception>
+        internal static double GetAOFunctionGenerationSettlingTime(this DAQmxTaskInformation taskInfo, string individualChannelString)
+        {
+            var period = 1 / taskInfo.Task.AOChannels[individualChannelString].FunctionGenerationFrequency;
+            var perDeviceSettlingTimes = new List<double>();
+            foreach (var device in taskInfo.Task.Devices)
+            {
+                var productType = DaqSystem.Local.LoadDevice(device).ProductType;
+                var settlingTime = 0.0;
+                switch (productType)
+                {
+                    case "PXIe-4467":
+                        // The PXIe-4467 has a settling time that is calculated as: 80 cycles + 30 ms.
+                        settlingTime = (period * 80.0) + 30e-3;
+                        break;
+                    case "PXIe-4468":
+                        // The PXIe-4467 has a settling time that is calculated as: 90 cycles + 1 ms.
+                        settlingTime = (period * 90.0) + 1e-3;
+                        break;
+                    default:
+                        throw new NIMixedSignalException($"The requested function is not supported by this device: {productType}");
+                }
+                perDeviceSettlingTimes.Add(settlingTime);
+            }
+            return perDeviceSettlingTimes.Max();
         }
     }
 }
---
+++ SemiconductorTestLibrary.Extensions/test/Unit/InstrumentAbstraction/DAQmx/AnalogOutputFunctionGenerationTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NationalInstruments.DAQmx;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DAQmx;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+using Xunit;
+using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+
+namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbstraction.DAQmx
+{
+    [Collection("NonParallelizable")]
+    public sealed class AnalogOutputFunctionGenerationTests : IDisposable
+    {
+        private ISemiconductorModuleContext _tsmContext;
+
+        public TSMSessionManager Initialize(string pinMapFileName)
+        {
+            _tsmContext = CreateTSMContext(pinMapFileName);
+            InitializeAndClose.CreateDAQmxAOFunctionGenerationTasks(_tsmContext);
+            return new TSMSessionManager(_tsmContext);
+        }
+
+        public void Dispose()
+        {
+            InitializeAndClose.ClearDAQmxAOVoltageTasks(_tsmContext);
+        }
+
+        [Fact]
+        public void SingleChannelSingleDevice_GenerateSineWave_Succeeds()
+        {
+            var sessionManager = Initialize("DAQmxTests.pinmap");
+            var tasksBundle = sessionManager.DAQmx("VCC1");
+
+            tasksBundle.Stop();
+            tasksBundle.Unreserve();
+            tasksBundle.ConfigureAOFunctionGeneration(new AOFunctionGenerationSettings
+            {
+                FunctionType = AOFunctionGenerationType.Sine,
+                Frequency = 1000,
+                Amplitude = 1,
+                Offset = 0,
+            });
+            tasksBundle.StartAOFunctionGeneration();
+        }
+    }
+}
