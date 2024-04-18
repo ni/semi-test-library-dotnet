862e48971b02ccb73f56ee2491d4e274c12245ea
Lorna Zhang <lorna.zhang@ni.com>
2024-04-18 14:02:31 +0800

    Add basic tests.

---
+++ TestAssets/Tests.SemiconductorTestLibrary.Utilities/InstrumentAbstraction/ScopeTests.cs
@@ -0,0 +1,22 @@
+﻿using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+using static NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Scope.InitializeAndClose;
+using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+
+namespace NationalInstruments.Tests.SemiconductorTestLibrary.InstrumentAbstraction
+{
+    public static class ScopeTests
+    {
+        public static TSMSessionManager TestSetup(string pinMapFileName, out ISemiconductorModuleContext tsmContext)
+        {
+            tsmContext = CreateTSMContext(pinMapFileName);
+            Initialize(tsmContext);
+            return new TSMSessionManager(tsmContext);
+        }
+
+        public static void TestCleanup(ISemiconductorModuleContext tsmContext)
+        {
+            Close(tsmContext);
+        }
+    }
+}
