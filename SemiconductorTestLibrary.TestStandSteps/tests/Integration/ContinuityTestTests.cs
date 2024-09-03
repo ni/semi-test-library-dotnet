6250cddecf3121c2256bdc3c236301334f08594f
Lorna Zhang <49468811+lornazh@users.noreply.github.com>
2024-09-03 16:41:33 +0800

    Fix ContinuityTest and add an integration test. (#116)
    Fixes issue with ContinuityTest to allow support for forcing negative current values.

---
+++ SemiconductorTestLibrary.TestStandSteps/tests/Integration/ContinuityTestTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.CommonSteps;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
+using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+
+namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
+{
+    [Collection("NonParallelizable")]
+    public class ContinuityTestTests
+    {
+        [Fact]
+        public void Initialize_RunContinuityTestWithNegativeCurrentLevel_Succeeds()
+        {
+            var tsmContext = CreateTSMContext("Mixed Signal Tests.pinmap", "Mixed Signal Tests.digiproj");
+            SetupNIDCPowerInstrumentation(tsmContext);
+
+           ContinuityTest(
+               tsmContext,
+               supplyPinsOrPinGroups: Array.Empty<string>(),
+               currentLimitsPerSupplyPinOrPinGroup: Array.Empty<double>(),
+               continuityPinsOrPinGroups: new string[] { "VCC1", "VCC2" },
+               currentLevelPerContinuityPinOrPinGroup: new double[] { -0.02, -0.01 },
+               voltageLimitHighPerContinuityPinOrPinGroup: new double[] { 1, 1 },
+               voltageLimitLowPerContinuityPinOrPinGroup: new double[] { -1, -1 },
+               apertureTime: 5e-5,
+               settlingTime: 5e-5);
+
+            CleanupInstrumentation(tsmContext);
+        }
+    }
+}
