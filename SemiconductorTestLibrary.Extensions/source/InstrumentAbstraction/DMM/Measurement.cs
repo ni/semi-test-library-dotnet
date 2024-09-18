222c048151bc41654979b3b0ee4db1791851d5f8
mattjet27 <11801997+Mattjet27@users.noreply.github.com>
2024-09-18 20:12:11 +0200

    Fix dmm mulipoint extensions by obsoleting existing mulipoint methods and adding new overloads

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DMM/Measurement.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DMM/Measurement.cs
@@ -1,5 +1,6 @@
 ﻿using NationalInstruments.SemiconductorTestLibrary.Common;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
+using System;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DMM
 {
@@ -64,17 +65,33 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DMM
         }
 
         /// <summary>
+        /// Fetches multiple points from previously initiated multipoint acquisition.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="DMMSessionsBundle"/> object.</param>
+        /// <param name="samplesToFetch">The number of values to fetch.</param>
+        /// <param name="maximumTimeInMilliseconds">The maximum time for the fetch to complete in milliseconds.</param>
+        /// <returns>The measurement results in per-site per-pin format.</returns>
+        public static PinSiteData<double[]> Fetch(this DMMSessionsBundle sessionsBundle, int samplesToFetch, double maximumTimeInMilliseconds)
+        {
+            return sessionsBundle.DoAndReturnPerSitePerPinResults(sessionInfo =>
+            {
+                return new double[][] { sessionInfo.Session.Measurement.FetchMultiPoint(PrecisionTimeSpan.FromMilliseconds(maximumTimeInMilliseconds), samplesToFetch) };
+            });
+        }
+
+        /// <summary>
         /// Fetches the specified number of values from previously initiated measurements.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="DMMSessionsBundle"/> object.</param>
-        /// <param name="numberToRead">The number of values to fetch.</param>
+        /// <param name="numberOfPoints">The number of points to fetch.</param>
         /// <param name="maximumTimeInMilliseconds">The maximum time for the fetch to complete in milliseconds.</param>
         /// <returns>The measurement results in per-instrument per-point format.</returns>
-        public static double[][] FetchMultiPoint(this DMMSessionsBundle sessionsBundle, int numberToRead, double maximumTimeInMilliseconds)
+        [Obsolete("This method is obsolete, use the Fetch(int numberOfPoints, double maximumTimeInMilliseconds) method instead.")]
+        public static double[][] FetchMultiPoint(this DMMSessionsBundle sessionsBundle, int numberOfPoints, double maximumTimeInMilliseconds)
         {
             return sessionsBundle.DoAndReturnPerInstrumentPerChannelResults(sessionInfo =>
             {
-                return sessionInfo.Session.Measurement.FetchMultiPoint(PrecisionTimeSpan.FromMilliseconds(maximumTimeInMilliseconds), numberToRead);
+                return sessionInfo.Session.Measurement.FetchMultiPoint(PrecisionTimeSpan.FromMilliseconds(maximumTimeInMilliseconds), numberOfPoints);
             });
         }
 
@@ -110,12 +127,28 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DMM
         }
 
         /// <summary>
+        /// Reads measurement results.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="DMMSessionsBundle"/> object.</param>
+        /// <param name="numberOfPoints">The number of points to read.</param>
+        /// <param name="maximumTimeInMilliseconds">The maximum time for the fetch to complete in milliseconds.</param>
+        /// <returns>The measurement results in per-site per-pin format.</returns>
+        public static PinSiteData<double[]> Read(this DMMSessionsBundle sessionsBundle, int numberOfPoints, double maximumTimeInMilliseconds)
+        {
+            return sessionsBundle.DoAndReturnPerSitePerPinResults(sessionInfo =>
+            {
+                return new double[][] { sessionInfo.Session.Measurement.ReadMultiPoint(PrecisionTimeSpan.FromMilliseconds(maximumTimeInMilliseconds), numberOfPoints) };
+            });
+        }
+
+        /// <summary>
         /// Reads specified number of measurement results.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="DMMSessionsBundle"/> object.</param>
         /// <param name="numberToRead">The number of values to fetch.</param>
         /// <param name="maximumTimeInMilliseconds">The maximum time for the fetch to complete in milliseconds.</param>
         /// <returns>The measurement results in per-instrument per-value format.</returns>
+        [Obsolete("This method is obsolete, use the Read(int numberOfPoints, double maximumTimeInMilliseconds) method instead.")]
         public static double[][] ReadMultiPoint(this DMMSessionsBundle sessionsBundle, int numberToRead, double maximumTimeInMilliseconds)
         {
             return sessionsBundle.DoAndReturnPerInstrumentPerChannelResults(sessionInfo =>
