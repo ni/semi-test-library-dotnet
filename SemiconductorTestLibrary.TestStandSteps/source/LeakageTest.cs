24227f8f1a68e902c4acf25fcce812037e858ec3
mattjet27 <11801997+Mattjet27@users.noreply.github.com>
2024-10-15 11:36:48 +0200

    Update the LeakageTest implementation to further align with the legacy implementation * Ensure 0V is forced after measuring, and the output is disabled/powered-down for all pins at once at the end of the test * Further condense down the code to be more efficient and read better * Add change to CHANGELOG.md

--- SemiconductorTestLibrary.TestStandSteps/source/LeakageTest.cs
+++ SemiconductorTestLibrary.TestStandSteps/source/LeakageTest.cs
@@ -15,13 +15,13 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
         /// Performs a single leakage measurement using the following test method:
         /// 1. The test first configures the instruments connected to the specified pins and ensures they are forcing 0V.
         /// 2. The specified voltage is then forced on each pin.
-        /// 3. After the specified settling time current is measured on each pin.
-        /// 4. Each pin is configured back to forcing 0V.
-        /// 5. The output on each pin is disabled.
+        /// 3. After the specified settling time, the current is measured on each pin.
+        /// 4. Each pin is then configured back to forcing 0V.
+        /// 5. Finally, the output on each pin is disabled.
         /// Pins mapped to either an NI SMU or NI PPMU(s) instrument channel are supported.
         /// By default, steps 2-4 are preformed in parallel, across all pins simultaneously.
         /// When the serialOperationEnabled Boolean input parameter is set to true, steps 2-4 are preformed serially, for each pin or pin group, one at a time.
-        /// Pin within Pin groups are always operated on in parallel.
+        /// Pins within pin groups are always operated on in parallel.
         /// The following Published Data Id is used for test results on a per-pin basis: Leakage.
         /// Note that the leakage current measurements are published separately for each pin, regardless of if a pin group is provided.
         /// Each test evaluation defined in the Test tab of the calling TestStand step must specify a pin.
@@ -29,8 +29,8 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
         /// </summary>
         /// <param name="tsmContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
         /// <param name="pinsOrPinGroups">The pins or pin groups.</param>
-        /// <param name="voltageLevel">The DC voltage level to force, in volts.</param>
-        /// <param name="currentLimit">The current limit in amperes.</param>
+        /// <param name="voltageLevel">The DC voltage level to force, in Volts.</param>
+        /// <param name="currentLimit">The current limit in Amperes.</param>
         /// <param name="apertureTime">The measurement aperture time in seconds.</param>
         /// <param name="settlingTime">The amount of time to wait before measuring the current, in seconds.</param>
         /// <param name="serialOperationEnabled">Whether to enforce testing sequentially across pins or pin groups.</param>
@@ -47,7 +47,7 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
             {
                 var sessionManager = new TSMSessionManager(tsmContext);
 
-                // Get <see cref="ISessionsBundle"/> objects associated with all <paramref name="pinsOrPinGroups"/>.
+                // Get the appropriate ISessionsBundle objects associated with all pins or pin groups.
                 DCPowerSessionsBundle dcPower = null;
                 DigitalSessionsBundle digital = null;
                 InvokeInParallel(
@@ -71,7 +71,7 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
                 var originalSourceDelays = dcPower?.GetSourceDelayInSeconds();
                 var dcPowerMeasureSettings = new DCPowerMeasureSettings() { ApertureTime = apertureTime };
 
-                // Configure initial settings and Force 0 V on all pins.
+                // Configure initial settings and force 0 V on all pins.
                 InvokeInParallel(
                     () =>
                     {
@@ -87,64 +87,66 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
                         if (digital != null)
                         {
                             digital.ConfigureApertureTime(apertureTime);
-                            digital.ForceVoltage(0, currentLimit);
-                            PreciseWait(settlingTime);
+                            digital.ForceVoltage(0, currentLimit, settlingTime: settlingTime);
                         }
                     });
 
-                // Force specified voltage either sequentially or in parallel according to <paramref name="serialOperationEnabled"/>.
+                // Force specified voltage either sequentially or in parallel according to the serialOperationEnabled parameter.
                 if (serialOperationEnabled)
                 {
-                    // Force specified voltage, measure current, then disable the output on each pin or pin group in serially, one pin or pin group at a time.
+                    // Force specified voltage, measure current, then force back to 0V, one pin or pin group at a time.
                     foreach (var pinOrPinGroup in pinsOrPinGroups)
                     {
                         var dcPowerPins = tsmContext.FilterPinsByInstrumentType(new string[] { pinOrPinGroup }, InstrumentTypeIdConstants.NIDCPower);
                         if (dcPowerPins.Any())
                         {
                             var dcPowerPerPinOrPinGroup = sessionManager.DCPower(dcPowerPins);
-                            dcPowerPerPinOrPinGroup.ForceVoltage(voltageLevel, currentLimit, waitForSourceCompletion: true);
+                            dcPowerPerPinOrPinGroup.ForceVoltage(voltageLevel, waitForSourceCompletion: true);
                             dcPowerPerPinOrPinGroup.MeasureAndPublishVoltage("Leakage", out _);
-                            dcPowerPerPinOrPinGroup.PowerDown();
+                            dcPowerPerPinOrPinGroup.ForceVoltage(0, waitForSourceCompletion: true);
                         }
 
                         var digitalPins = tsmContext.FilterPinsByInstrumentType(new string[] { pinOrPinGroup }, InstrumentTypeIdConstants.NIDigitalPattern);
                         if (digitalPins.Any())
                         {
                             var digitalPerPinOrPinGroup = sessionManager.Digital(digitalPins);
-                            digitalPerPinOrPinGroup.ForceVoltage(voltageLevel, currentLimit);
-                            PreciseWait(settlingTime);
+                            digitalPerPinOrPinGroup.ForceVoltage(voltageLevel, settlingTime: settlingTime);
                             digitalPerPinOrPinGroup.MeasureAndPublishVoltage("Leakage", out _);
-                            digitalPerPinOrPinGroup.TurnOffOutput();
+                            digitalPerPinOrPinGroup.ForceVoltage(0, settlingTime: settlingTime);
                         }
                     }
                 }
                 else
                 {
-                    // Force specified voltage on all pins simultaneously.
+                    // Force specified voltage on all pins simultaneously, and wait for settling.
                     InvokeInParallel(
-                        () => dcPower?.ForceVoltage(voltageLevel, currentLimit, waitForSourceCompletion: true),
-                        () =>
-                        {
-                            if (digital != null)
-                            {
-                                digital.ForceVoltage(voltageLevel, currentLimit);
-                                PreciseWait(settlingTime);
-                            }
-                        });
+                        () => dcPower?.ForceVoltage(voltageLevel, waitForSourceCompletion: true),
+                        () => digital?.ForceVoltage(voltageLevel, settlingTime: settlingTime));
 
                     // Measure current on all pins simultaneously.
                     InvokeInParallel(
                         () => dcPower?.MeasureAndPublishCurrent("Leakage", out _),
                         () => digital?.MeasureAndPublishCurrent("Leakage", out _));
 
-                    // Disable output on all pins simultaneously.
+                    // Force pins back to 0V simultaneously, and wait for settling.
                     InvokeInParallel(
-                        () => dcPower?.PowerDown(),
-                        () => digital?.TurnOffOutput());
+                        () => dcPower?.ForceVoltage(0, waitForSourceCompletion: true),
+                        () => digital?.ForceVoltage(0, settlingTime: settlingTime));
                 }
 
-                // Restore Source Delay back to its original value.
-                dcPower?.ConfigureSourceDelay(originalSourceDelays);
+                // Disable output on all pins simultaneously.
+                // Do not need to wait for settling, since the output of all pins is already set 0V.
+                // Ensure the source delay property is restored back to its original value.
+                InvokeInParallel(
+                    () =>
+                    {
+                        if (dcPower != null)
+                        {
+                            dcPower.ConfigureSourceDelay(originalSourceDelays);
+                            dcPower.PowerDown();
+                        }
+                    },
+                    () => digital?.TurnOffOutput());
             }
             catch (Exception e)
             {
