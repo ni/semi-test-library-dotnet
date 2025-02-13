6caaf19c1d147910b574c697736dd357d226bcf8
Shalini Subramanian <shalini.subramanian@emerson.com>
2025-02-13 13:59:24 +0800

    fix: Update PR comments
    Signed-off-by: Shalini Subramanian <shalini.subramanian@emerson.com>

--- Examples/tests/Integration/PatternTriggeredInstrumentControl.cs
+++ Examples/tests/Integration/PatternTriggeredInstrumentControl.cs
@@ -7,7 +7,7 @@ using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMCon
 namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
 {
     [Collection("NonParallelizable")]
-    public sealed class PatternTriggeredInstrumentControl
+    public class PatternTriggeredInstrumentControl
     {
         [Fact]
         public void Initialize_RunPatternTriggeredSmuMeasurement_SucceedsAndPublishesData()
