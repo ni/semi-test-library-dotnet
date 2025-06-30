4fe10d83e9beb664167f2c37eabf32d87eff9403
Ashutosh.Mishra <Ashutosh.Mishra@ni.com>
2025-06-30 21:52:40 +0530

    Addition of pinmaps and updating auto test

--- SemiconductorTestLibrary.TestStandSteps/tests/Cascade/MergeTypeCascadeTests.cs
+++ SemiconductorTestLibrary.TestStandSteps/tests/Cascade/MergeTypeCascadeTests.cs
@@ -1,79 +1,78 @@
-﻿using NationalInstruments.ModularInstruments.NIDCPower;
+﻿using System.Collections.Generic;
+using NationalInstruments.ModularInstruments.NIDCPower;
 using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.Tests.SemiconductorTestLibrary.Utilities;
 using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
 using Xunit;
-using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
 using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.CommonSteps;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
 using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
 
 namespace NationalInstruments.Tests.SemiconductorTestLibrary.Cascade
 {
     public class MergeTypeCascadeTests
     {
-        private readonly ISemiconductorModuleContext _tsmContext;
-        public MergeTypeCascadeTests()
+        public static IEnumerable<object[]> GetPinGroupsSMU4163()
         {
-            _tsmContext = CreateTSMContext("pinmap"); // TODO : add the pinmap
+            List<string> pinGroupNames = new List<string> { "G1_1mA", "G2_1mA", "G3_1mA", "G4_1mA", "G1_2mA", "G2_2mA", "G1_4mA" };
+
+            foreach (var pinGroupName in pinGroupNames)
+            {
+                yield return new object[] { pinGroupName };
+            }
         }
 
         [Theory]
-        [InlineData("TwoPinPinGroup")]
-        [InlineData("FourPinPinGroup")]
-        public void InitializeSMU4147_MergePinGroupAndForceCurrentMeasureVoltage(string pinGroup)
+        [InlineData("G1_2mA")]
+        [InlineData("G2_2mA")]
+        [InlineData("G1_4mA")]
+        [Trait(nameof(Platform), nameof(Platform.TesterOnly))]
+        [Trait(nameof(HardwareConfiguration), nameof(HardwareConfiguration.Lungyuan))]
+        public void InitializeSMU4147_MergePinGroupForceCurrentMeasureVoltageAndUnMerge(string pinGroup)
         {
-            SetupNIDigitalPatternInstrumentation(_tsmContext);
-            SetupNIDCPowerInstrumentation(_tsmContext, measurementSense: DCPowerMeasurementSense.Local);
+            var tsmContext = CreateTSMContext("Merged_4147.pinmap");
+            SetupNIDCPowerInstrumentation(tsmContext, measurementSense: DCPowerMeasurementSense.Local);
 
-            var sessionManager = new TSMSessionManager(_tsmContext);
-            var dcPower = sessionManager.DCPower(new[] { "PowerPins" });
-            dcPower.MergePinGroup(pinGroup);
-            ForceCurrentMeasureVoltage(
-                _tsmContext,
-                pinsOrPinGroups: new[] { "MergedPowerPins" },
-                currentLevel: 0.005,
-                voltageLimit: 3.3,
-                apertureTime: 5e-5,
-                settlingTime: 5e-5);
-            dcPower.UnmergePinGroup(pinGroup);
+            MergePinGroupForceCurrentMeasureVoltageAndUnMerge(tsmContext, pinGroup);
+            CleanupInstrumentation(tsmContext);
         }
 
         [Theory]
-        [InlineData("TwoPinPinGroup")]
-        [InlineData("FourPinPinGroup")]
-        [InlineData("EightPinPinGroup")]
-        public void InitializeSMU4162_MergePinGroupAndForceCurrentMeasureVoltage(string pinGroup)
+        [InlineData("G1_2mA")]
+        [InlineData("G2_2mA")]
+        [InlineData("G1_4mA")]
+        [Trait(nameof(Platform), nameof(Platform.TesterOnly))]
+        [Trait(nameof(HardwareConfiguration), nameof(HardwareConfiguration.Lungyuan))]
+        public void InitializeSMU4162_MergePinGroupForceCurrentMeasureVoltageAndUnMerge(string pinGroup)
         {
-            SetupNIDigitalPatternInstrumentation(_tsmContext);
-            SetupNIDCPowerInstrumentation(_tsmContext, measurementSense: DCPowerMeasurementSense.Local);
+            var tsmContext = CreateTSMContext("Merged_4162.pinmap");
+            SetupNIDCPowerInstrumentation(tsmContext, measurementSense: DCPowerMeasurementSense.Local);
 
-            var sessionManager = new TSMSessionManager(_tsmContext);
-            var dcPower = sessionManager.DCPower(new[] { "PowerPins" });
-            dcPower.MergePinGroup(pinGroup);
-            ForceCurrentMeasureVoltage(
-                _tsmContext,
-                pinsOrPinGroups: new[] { "MergedPowerPins" },
-                currentLevel: 0.005,
-                voltageLimit: 3.3,
-                apertureTime: 5e-5,
-                settlingTime: 5e-5);
-            dcPower.UnmergePinGroup(pinGroup);
+            MergePinGroupForceCurrentMeasureVoltageAndUnMerge(tsmContext, pinGroup);
+            CleanupInstrumentation(tsmContext);
         }
 
         [Theory]
-        [InlineData("TwoPinPinGroup")]
-        [InlineData("FourPinPinGroup")]
-        [InlineData("EightPinPinGroup")]
-        public void InitializeSMU4167_MergePinGroupAndForceCurrentMeasureVoltage(string pinGroup)
+        [MemberData(nameof(GetPinGroupsSMU4163))]
+        [Trait(nameof(Platform), nameof(Platform.TesterOnly))]
+        [Trait(nameof(HardwareConfiguration), nameof(HardwareConfiguration.Lungyuan))]
+        public void InitializeSMU4163_MergePinGroupForceCurrentMeasureVoltageAndUnMerge(string pinGroup)
         {
-            SetupNIDigitalPatternInstrumentation(_tsmContext);
-            SetupNIDCPowerInstrumentation(_tsmContext, measurementSense: DCPowerMeasurementSense.Local);
+            var tsmContext = CreateTSMContext("Merged_4163.pinmap");
+            SetupNIDCPowerInstrumentation(tsmContext, measurementSense: DCPowerMeasurementSense.Local);
 
-            var sessionManager = new TSMSessionManager(_tsmContext);
-            var dcPower = sessionManager.DCPower(new[] { "PowerPins" });
+            MergePinGroupForceCurrentMeasureVoltageAndUnMerge(tsmContext, pinGroup);
+            CleanupInstrumentation(tsmContext);
+        }
+
+        private void MergePinGroupForceCurrentMeasureVoltageAndUnMerge(ISemiconductorModuleContext tsmContext, string pinGroup)
+        {
+            var sessionManager = new TSMSessionManager(tsmContext);
+            var dcPower = sessionManager.DCPower(new[] { "G1_4mA" });
             dcPower.MergePinGroup(pinGroup);
             ForceCurrentMeasureVoltage(
-                _tsmContext,
-                pinsOrPinGroups: new[] { "MergedPowerPins" },
+                tsmContext,
+                pinsOrPinGroups: new[] { pinGroup },
                 currentLevel: 0.005,
                 voltageLimit: 3.3,
                 apertureTime: 5e-5,
