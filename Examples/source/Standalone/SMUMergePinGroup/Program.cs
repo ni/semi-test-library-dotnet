8d75c8908882f4881738203e31b11eaf79732f51
தே.க.தமிழ் <176882975+nitamil@users.noreply.github.com>
2025-07-07 20:43:08 +0530

    Update Program.cs

--- Examples/source/Standalone/SMUMergePinGroup/Program.cs
+++ Examples/source/Standalone/SMUMergePinGroup/Program.cs
@@ -19,81 +19,94 @@ namespace NationalInstruments.SemiconductorTestLibrary.Examples.StandAlone.SMUMe
     /// <remarks>
     /// Using this example:
     /// 1. Expand the Settings and Configuration region and familiarize yourself with the settings.
-    /// 2. Ensure the resource names within the PinMap.pinmap file match the resource name of the NI-Digital Pattern Instrument in your system.
-    ///    The default value for the resource name is "HSD_6571_C1_S02".
+    /// 2. Ensure the resource names within the PinMap.pinmap file match the resource name of the NI-DCPower Instrument in your system.
+    ///    The default value for the resource name is "SMU_4147_C1_S05".
     /// 3. Note that only few DCPower Instruments support merging feature which includes PXIe-4147, PXIe-4162 and PXIe-4163.
     /// This also requires the pinmap to define the pingroup to contain all the pins to be merged and each
     /// pin is mapped to SMU channels of same module.
     /// This example assumes:
     ///  - Relay configurations to be applied before the merging operation ensures the SMUs channels are connected
     ///  in parallel configuration and disconnect after unmerging.
+    ///  - The relay operations are skipped and they are provided as code template purpose only.
     /// 4. Build and run the example.
     /// </remarks>
-    public static partial class ExampleTestSteps
+    public sealed class Program
     {
+        #region Settings and Configuration
+
+        //Relay configuration that connects all the channels in parallel
+        private const string ConnectedRelayConfiguration = "ConnectedRelayConfig";
+        //Relay configuration that disconnects all the channels
+        private const string DisconnectedRelayConfiguration = "DisconnectedRelayConfig";
+
+        // PinGroups for merging.
+        private const string Vcc5A = "Vcc5A";  //Name of the pin group to be merged for sourcing 5A
+        private const string Vcc10A = "Vcc10A";  //Name of the pin group to be merged for sourcing 10A
+
+        // voltage and current settings for the merge operation
+        private const double currentLevel1 = 5.0;
+        private const double currentLevel2 = 10.0;
+        private const double voltageLimit = 0.4;
+
+        // settling and aperture time used for measurements
+        private static readonly double SettlingTime = 0.001;
+        private static readonly double ApertureTimeConstant = -1; // -1 indicates no aperture time is set.
+
+        // Files to load.
+        private const string PinMapFileName = "PinMap.pinmap";
+        #endregion Settings and Configuration
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "NI1704:Identifiers should be spelled correctly", Justification = "ExampleCode")]
+
         /// <summary>
         /// Merges the specified pin group to force high current and measure voltage for pins mapped to DCPower Instruments from same module.
         /// Specifically, this method merges the pin group, forces a voltage level, measures the current, and then unmerges the pin group.
         /// </summary>
         /// <param name="tsmContext">Teststand Semiconductor module context</param>
-        /// <param name="pinGroup">Name of the pin group to be merged</param>
-        /// <param name="voltageLevel">Voltage level to set output</param>
-        /// <param name="currentLimit">Current limit for output</param>
-        /// <param name="settlingTime">Settling time used for measurements</param>
-        /// <param name="apertureTime">Aperture time used for measurements</param>
-        /// <param name="connectedRelayConfiguration">Relay configuration that connects all the channels in parallel</param>
-        /// <param name="disconnectedRelayConfiguration">Relay configuration that disconnects all the channels</param>
         /// <returns>PinSiteData measurement  in double precision </returns>
         public static void SMUMergeToForceHighCurrent(
-            ISemiconductorModuleContext tsmContext,
-            string pinGroup,
-            double voltageLevel = 3.3,
-            double currentLimit = 0.01,
-            double settlingTime = 0.001,
-            double apertureTime = -1,
-            string connectedRelayConfiguration = "",
-            string disconnectedRelayConfiguration = "")
+            ISemiconductorModuleContext tsmContext)
         {
             TSMSessionManager sessionManager = new TSMSessionManager(tsmContext);
-            DCPowerSessionsBundle smuBundle = sessionManager.DCPower(pinGroup);
+            DCPowerSessionsBundle smuBundle = sessionManager.DCPower(Vcc5A);
 
             // Configure the instrumentation connected to the target pins
-            if (!connectedRelayConfiguration.IsEmpty())
+            if (!ConnectedRelayConfiguration.IsEmpty())
             {
                 // Configure the relays required for merging.
-                tsmContext.ApplyRelayConfiguration(connectedRelayConfiguration, waitSeconds: settlingTime);
+                tsmContext.ApplyRelayConfiguration(ConnectedRelayConfiguration, waitSeconds: SettlingTime);
             }
             // Store the current source delay settings as a backup to restore later.
             PinSiteData<double> originalSourceDelays = smuBundle.GetSourceDelayInSeconds();
 
             // Perform merge operation on the pin group.
-            smuBundle.MergePinGroup(pinGroup);
-            smuBundle.ConfigureSourceDelay(settlingTime);
-            if (apertureTime != -1)
+            smuBundle.MergePinGroup(Vcc5A);
+            smuBundle.ConfigureSourceDelay(SettlingTime);
+            if (ApertureTimeConstant != -1)
             {
                 DCPowerMeasureSettings measureSettings = new DCPowerMeasureSettings()
                 {
-                    ApertureTime = apertureTime,
+                    ApertureTime = ApertureTimeConstant,
                 };
                 smuBundle.ConfigureMeasureSettings(measureSettings);
             }
             
             // Source and/or measure the signals.
-            smuBundle.ForceVoltage(voltageLevel, currentLimit, waitForSourceCompletion: true);
-            PreciseWait(timeInSeconds: settlingTime);
+            smuBundle.ForceCurrent(currentLimit, voltageLevel, waitForSourceCompletion: true);
+            PreciseWait(timeInSeconds: SettlingTime);
             smuBundle.MeasureAndPublishCurrent(publishedDataId: "Current");
             
             // Clean up and restore the state of the instrumentation after finishing the test.
             smuBundle.ForceVoltage(voltageLevel: 0, currentLimit: 0.001);
             smuBundle.PowerDown();
-            PreciseWait(timeInSeconds: settlingTime);
+            PreciseWait(timeInSeconds: SettlingTime);
 
             // Use the SMU Bundle object to perform unmerge operation on the pin group and disconnect the relays.
-            smuBundle.UnmergePinGroup(pinGroup);
-            if (!disconnectedRelayConfiguration.IsEmpty())
+            smuBundle.UnmergePinGroup(Vcc5A);
+            if (!DisconnectedRelayConfiguration.IsEmpty())
             {
                 // Configure the relays required for unmerging.
-                tsmContext.ApplyRelayConfiguration(disconnectedRelayConfiguration, waitSeconds: settlingTime);
+                tsmContext.ApplyRelayConfiguration(DisconnectedRelayConfiguration, waitSeconds: SettlingTime);
             }
             // Restore the source delay to original value.
             smuBundle.ConfigureSourceDelay(originalSourceDelays);
