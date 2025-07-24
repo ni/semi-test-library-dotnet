127e837733c47983159800c91c105d77a80af8fa
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2025-07-24 19:05:03 +0530

    Updated NameSpace again and rebuilt CustomInstrument

--- Examples/source/Sequence/CustomInstrument/Code Modules/TestStep.cs
+++ Examples/source/Sequence/CustomInstrument/Code Modules/TestStep.cs
@@ -1,11 +1,11 @@
 ﻿using System.Linq;
+using NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument.MyCustomInstrument;
 using NationalInstruments.SemiconductorTestLibrary.Common;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
-using NationalInstruments.SemiconductorTestLibrary.Examples.CustomInstrument.MyCustomInstrument;
 using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
 using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
 
-namespace NationalInstruments.SemiconductorTestLibrary.Examples.CustomInstrument
+namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
 {
     /// <summary>
     /// This class contains sample methods to perform high-level testing of the DUT using the custom instrument support provided by STL.
