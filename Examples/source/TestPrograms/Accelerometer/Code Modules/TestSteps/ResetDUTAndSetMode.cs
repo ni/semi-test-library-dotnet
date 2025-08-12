636299ecd30b30cbfb7e1f83cadc5d0907cc4931
Matthew Roberts <11801997+Mattjet27@users.noreply.github.com>
2025-08-12 15:54:34 +0200

    Add Accelerometer Test Program Style Example (#317)
    Initial upload of Accelerometer Test Program Style example, a modified version of the accelerometer example that ships with the NI TestStand Semiconductor Module (TSM) updated to use the Semiconductor Test Library (STL)

---
+++ Examples/source/TestPrograms/Accelerometer/Code Modules/TestSteps/ResetDUTAndSetMode.cs
@@ -0,0 +1,43 @@
+﻿using NationalInstruments.SemiconductorTestLibrary.Common;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Digital;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+
+namespace NationalInstruments.Examples.SemiconductorTestLibrary.Accelerometer
+{
+    /// <summary>
+    /// Partial class containing all test steps for the project.
+    /// This is declared as a partial class so that test code modules can be managed as unique methods in separate files.
+    /// </summary>
+    public static partial class TestSteps
+    {
+        /// <summary>
+        /// Resets the DUT and sets the test mode.
+        /// </summary>
+        /// <param name="semiconductorModuleContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
+        /// <param name="rstPinName">The DUT's RST pin name, as defined in the pin map file.</param>
+        /// <param name="modePinName">The DUT's Test Mode pin name, as defined in the pin map file.</param>
+        public static void ResetDUTAndSetMode(ISemiconductorModuleContext semiconductorModuleContext, string rstPinName, string modePinName)
+        {
+            double vih = semiconductorModuleContext.GetSpecificationsValue("DC.Vih");
+            double vil = semiconductorModuleContext.GetSpecificationsValue("DC.Vil");
+            double rstPinSettlingTime = semiconductorModuleContext.GetSpecificationsValue("SettlingTimes.SetRST");
+            double modePinSettlingTime = semiconductorModuleContext.GetSpecificationsValue("SettlingTimes.SetTestMode");
+            double resetTimeInSeconds = semiconductorModuleContext.GetSpecificationsValue("AC.ResetTime");
+            double currentLimitRange = 32e-3;
+
+            var sessionManager = new TSMSessionManager(semiconductorModuleContext);
+            DigitalSessionsBundle rstPin = sessionManager.Digital(rstPinName);
+            DigitalSessionsBundle modePin = sessionManager.Digital(modePinName);
+
+            rstPin.ForceVoltage(vih, currentLimitRange, settlingTime: rstPinSettlingTime);
+
+            // This is separate from and in addtion to the rstPin settling time.
+            Utilities.PreciseWait(resetTimeInSeconds);
+
+            // Current limit range already set, no need to set it again.
+            rstPin.ForceVoltage(vil, settlingTime: rstPinSettlingTime);
+            modePin.ForceVoltage(vih, currentLimitRange, settlingTime: modePinSettlingTime);
+        }
+    }
+}
---
+++ Examples/source/TestPrograms/Accelerometer/Code Modules/TestSteps/WaitForTemperature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using NationalInstruments.Examples.SemiconductorTestLibrary.Accelerometer.Common;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+using static NationalInstruments.Examples.SemiconductorTestLibrary.Accelerometer.Common.Constants;
+
+namespace NationalInstruments.Examples.SemiconductorTestLibrary.Accelerometer
+{
+    /// <summary>
+    /// Partial class containing all test steps for the project.
+    /// This is declared as a partial class so that test code modules can be managed as unique methods in separate files.
+    /// </summary>
+    public static partial class TestSteps
+    {
+        /// <summary>
+        /// Waits for the temperature to reach the configured temperature value.
+        /// </summary>
+        /// <param name="semiconductorModuleContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
+        /// <exception cref="TimeoutException">
+        /// This exception is thrown if the configured temperature is not reached within the timeout limit (15 seconds).
+        /// </exception>
+        public static void WaitForTemperature(ISemiconductorModuleContext semiconductorModuleContext)
+        {
+            var temperatureController = semiconductorModuleContext.GetGlobalData<SimulatedTemperatureController>(TemperatureControllerDataId);
+
+            const int timeoutSeconds = 15;
+            DateTime endTime = DateTime.Now.AddSeconds(timeoutSeconds);
+            while (DateTime.Now < endTime)
+            {
+                if (temperatureController.CurrentTemperature == temperatureController.RequestedTemperature)
+                {
+                    return;
+                }
+                Thread.Sleep(100);
+            }
+            throw new TimeoutException("Timeout occurred waiting for temperature controller to reach desired temperature.");
+        }
+    }
+}
