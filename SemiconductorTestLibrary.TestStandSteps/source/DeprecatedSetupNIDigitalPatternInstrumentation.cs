3a2c19d47774a2e4b6dd06c8773c2088ad7fdb57
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2025-10-06 21:48:57 +0530

    Creating deprecated Enum and methods under Separate NameSpace.

---
+++ SemiconductorTestLibrary.TestStandSteps/source/DeprecatedSetupNIDigitalPatternInstrumentation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+
+namespace NationalInstruments.SemiconductorTestLibrary.TestStandStepsDeprecated
+{
+    /// <summary>
+    /// [Deprecated] SetupAndCleanupSteps class.
+    /// </summary>
+    public static partial class SetupAndCleanupSteps
+    {
+        /// <summary>
+        /// [Deprecated] This method is deprecated and use the other overload instead.
+        /// </summary>
+        /// <param name="tsmContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
+        /// <param name="resetDevice">Whether to reset device during initialization.</param>
+        /// <param name="levelsSheetToApply">The name of the levels sheet to apply.</param>
+        /// <param name="timingSheetToApply">The name of the timing sheet to apply.</param>
+        [Obsolete("Use TestStandSteps.NIInstrumentType instead.")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void SetupNIDigitalPatternInstrumentation(
+            ISemiconductorModuleContext tsmContext,
+            bool resetDevice = false,
+            string levelsSheetToApply = "",
+            string timingSheetToApply = "")
+        {
+            TestStandSteps.SetupAndCleanupSteps.SetupNIDigitalPatternInstrumentation(
+            tsmContext,
+            resetDevice,
+            levelsSheetToApply,
+            timingSheetToApply,
+            false);
+        }
+    }
+}
---
+++ SemiconductorTestLibrary.TestStandSteps/source/Deprecated_CleanupInstrumentation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+
+namespace NationalInstruments.SemiconductorTestLibrary.TestStandStepsDeprecated
+{
+    /// <summary>
+    /// [Deprecated]SetupAndCleanupSteps.
+    /// </summary>
+    public static partial class SetupAndCleanupSteps
+    {
+        /// <summary>
+        /// [Deprecated]NIInstrumentType.
+        /// </summary>
+        public enum NIInstrumentType
+        {
+            /// <summary>
+            /// All NI instruments.
+            /// </summary>
+            All,
+
+            /// <summary>
+            /// An NI-DCPower instrument.
+            /// </summary>
+            NIDCPower,
+
+            /// <summary>
+            /// An NI-Digital Pattern instrument.
+            /// </summary>
+            NIDigitalPattern,
+
+            /// <summary>
+            /// A relay driver module (NI-SWITCH instrument).
+            /// </summary>
+            NIRelayDriver,
+
+            /// <summary>
+            /// An NI-DAQmx task.
+            /// </summary>
+            NIDAQmx,
+
+            /// <summary>
+            /// An NI-DMM instrument.
+            /// </summary>
+            NIDMM,
+
+            /// <summary>
+            /// An NI-FGEN instrument.
+            /// </summary>
+            NIFGen,
+
+            /// <summary>
+            /// An NI-SCOPE instrument.
+            /// </summary>
+            NIScope,
+
+            /// <summary>
+            /// An NI-Sync instrument.
+            /// </summary>
+            NISync
+        }
+
+        /// <summary>
+        /// [Deprecated]CleanupInstrumentation.
+        /// </summary>
+        /// <param name="tsmContext">a</param>
+        /// <param name="resetDevice">a</param>
+        /// <param name="instrumentType">a</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("This overload is deprecated. Use the overload without optional parameter.", error: true)]
+        public static void CleanupInstrumentation(
+            ISemiconductorModuleContext tsmContext,
+            bool resetDevice = false,
+            NIInstrumentType instrumentType = NIInstrumentType.All)
+        {
+            TestStandSteps.SetupAndCleanupSteps.CleanupInstrumentation(
+            tsmContext,
+            resetDevice,
+            (TestStandSteps.NIInstrumentType)instrumentType);
+        }
+    }
+}
