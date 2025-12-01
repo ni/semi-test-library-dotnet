5145dd816e99fa6a53a3194009814cdbc7365496
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2025-12-01 20:26:50 +0530

    Updated and used RSeries instead of Rseries every where

--- Examples/source/Sequence/CustomInstrument.RSeries7822R/Code Modules/Imports/RSeries7822RDriverAPI/source/RSeriesDriver.cs
+++ Examples/source/Sequence/CustomInstrument.RSeries7822R/Code Modules/Imports/RSeries7822RDriverAPI/source/RSeriesDriver.cs
@@ -3,7 +3,7 @@
 namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
 {
     /// <summary>
-    /// RSeries driver class which provides .NET APIs for Rseries card driver operations.
+    /// RSeries driver class which provides .NET APIs for RSeries card driver operations.
     /// </summary>
     public class RSeriesDriver
     {
@@ -11,10 +11,10 @@ namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
         private int Status { get; set; }
 
         /// <summary>
-        /// Initializes Rseries card driver session.
+        /// Initializes RSeries card driver session.
         /// </summary>
         /// <param name="resourceName">InstrumentName.</param>
-        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception">Thrown when 'OpenFPGA' fails.</exception>
         public RSeriesDriver(string resourceName)
         {
             string bitFilePath = RSeriesCAPI.BitFilePath();
@@ -22,20 +22,20 @@ namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
             ReferenceID = fpgaRef;
             if (Status != 0)
             {
-                throw new Exception($"Error in Open FPGA Ref, ErrorCode:{Status}, ResourceName:{resourceName}, Ref:{ReferenceID}, Bitfile:{bitFilePath}");
+                throw new Exception($"Error in OpenFPGA Ref, ErrorCode:{Status}, ResourceName:{resourceName}, Ref:{ReferenceID}, Bitfile:{bitFilePath}");
             }
         }
 
         /// <summary>
-        /// Close Rseries card driver session.
+        /// Close RSeries card driver session.
         /// </summary>
-        /// <exception cref="Exception">Exception message.</exception>
+        /// <exception cref="Exception">Thrown when 'CloseFPGA' fails.</exception>
         public void Close()
         {
             Status = RSeriesCAPI.CloseFPGA(ReferenceID);
             if (Status != 0)
             {
-                throw new Exception($"Error in Close FPGA Ref, ErrorCode:{Status}");
+                throw new Exception($"Error in CloseFPGA Ref, ErrorCode:{Status}");
             }
         }
 
@@ -48,11 +48,11 @@ namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
         }
 
         /// <summary>
-        /// Rseries card write channel data operation.
+        /// RSeries card write channel data operation.
         /// </summary>
         /// <param name="channelString">ChannelName.</param>
         /// <param name="pinSiteSpecificData">ChannelData.</param>
-        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception">Thrown when FPGA 'WriteData' fails.</exception>
         public void WriteChannelData(string channelString, double pinSiteSpecificData)
         {
             Status = RSeriesCAPI.WriteData(ReferenceID, channelString, (byte)pinSiteSpecificData);
@@ -63,11 +63,11 @@ namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
         }
 
         /// <summary>
-        /// Rseries card read channel data operation.
+        /// RSeries card read channel data operation.
         /// </summary>
         /// <param name="channelString">Channel name.</param>
         /// <returns>Channel data.</returns>
-        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception">Thrown when FPGA 'ReadData' fails.</exception>
         public double MeasureChannelData(string channelString)
         {
             Status = RSeriesCAPI.ReadData(ReferenceID, channelString, out byte data);
@@ -79,10 +79,10 @@ namespace NationalInstruments.Example.CustomInstrument.RSeries7822DriverAPI
         }
 
         /// <summary>
-        /// Rseries card driver method for configure.
+        /// RSeries card driver method for configure.
         /// </summary>
         /// <param name="operationMode">Operation mode.</param>
-        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception">Thrown when 'EnableLoopBack' fails. </exception>
         public void ConfigureMode(string operationMode)
         {
             if (operationMode == "LoopBack")
