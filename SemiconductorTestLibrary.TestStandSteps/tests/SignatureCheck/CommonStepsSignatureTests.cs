bf67ac0918f94e8a3ce4b79743bb1cabff6751ef
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2026-01-20 22:31:50 +0530

    Added Signature check tests for force voltage and current related APIs

--- SemiconductorTestLibrary.TestStandSteps/tests/SignatureCheck/CommonStepsSignatureTests.cs
+++ SemiconductorTestLibrary.TestStandSteps/tests/SignatureCheck/CommonStepsSignatureTests.cs
@@ -246,5 +246,161 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.SignatureCheck
 
             Assert.Single(overloads);
         }
+
+        [Fact]
+        public void GetForceCurrentMeasureVoltageWithParameters_HasCorrectSignature()
+        {
+            var classType = typeof(CommonSteps);
+            var parameterTypes = new[]
+            {
+                typeof(ISemiconductorModuleContext),
+                typeof(string[]),
+                typeof(double),
+                typeof(double),
+                typeof(double),
+                typeof(double)
+            };
+            var method = classType.GetMethod(
+                "ForceCurrentMeasureVoltage",
+                BindingFlags.Public | BindingFlags.Static,
+                binder: null,
+                parameterTypes,
+                modifiers: null);
+
+            Assert.NotNull(method);
+            var parameters = method.GetParameters();
+            AssertParameter(parameters[0], "tsmContext", false);
+            AssertParameter(parameters[1], "pinsOrPinGroups", false);
+            AssertParameter(parameters[2], "currentLevel", false);
+            AssertParameter(parameters[3], "voltageLimit", false);
+            AssertDoubleParameter(parameters[4], "settlingTime", true, 0);
+            AssertDoubleParameter(parameters[5], "apertureTime", true, -1);
+            Assert.Equal(typeof(void), method.ReturnType);
+        }
+
+        [Fact]
+        public void GetAllForceCurrentMeasureVoltageOverloads_HasSingleOverload()
+        {
+            var classType = typeof(CommonSteps);
+            var overloads = classType.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "ForceCurrentMeasureVoltage");
+
+            Assert.Single(overloads);
+        }
+
+        [Fact]
+        public void GetForceVoltageMeasureCurrentWithParameters_HasCorrectSignature()
+        {
+            var classType = typeof(CommonSteps);
+            var parameterTypes = new[]
+            {
+                typeof(ISemiconductorModuleContext),
+                typeof(string[]),
+                typeof(double),
+                typeof(double),
+                typeof(double),
+                typeof(double)
+            };
+            var method = classType.GetMethod(
+                "ForceVoltageMeasureCurrent",
+                BindingFlags.Public | BindingFlags.Static,
+                binder: null,
+                parameterTypes,
+                modifiers: null);
+
+            Assert.NotNull(method);
+            var parameters = method.GetParameters();
+            AssertParameter(parameters[0], "tsmContext", false);
+            AssertParameter(parameters[1], "pinsOrPinGroups", false);
+            AssertParameter(parameters[2], "voltageLevel", false);
+            AssertParameter(parameters[3], "currentLimit", false);
+            AssertDoubleParameter(parameters[4], "settlingTime", true, 0);
+            AssertDoubleParameter(parameters[5], "apertureTime", true, -1);
+            Assert.Equal(typeof(void), method.ReturnType);
+        }
+
+        [Fact]
+        public void GetAllForceVoltageMeasureCurrentOverloads_HasSingleOverload()
+        {
+            var classType = typeof(CommonSteps);
+            var overloads = classType.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "ForceVoltageMeasureCurrent");
+
+            Assert.Single(overloads);
+        }
+
+        [Fact]
+        public void GetForceDcCurrentWithParameters_HasCorrectSignature()
+        {
+            var classType = typeof(CommonSteps);
+            var parameterTypes = new[]
+            {
+                typeof(ISemiconductorModuleContext),
+                typeof(string[]),
+                typeof(double),
+                typeof(double),
+                typeof(double)
+            };
+            var method = classType.GetMethod(
+                "ForceDcCurrent",
+                BindingFlags.Public | BindingFlags.Static,
+                binder: null,
+                parameterTypes,
+                modifiers: null);
+
+            Assert.NotNull(method);
+            var parameters = method.GetParameters();
+            AssertParameter(parameters[0], "tsmContext", false);
+            AssertParameter(parameters[1], "pinsOrPinGroups", false);
+            AssertParameter(parameters[2], "currentLevel", false);
+            AssertParameter(parameters[3], "voltageLimit", false);
+            AssertDoubleParameter(parameters[4], "settlingTime", true, 0);
+            Assert.Equal(typeof(void), method.ReturnType);
+        }
+
+        [Fact]
+        public void GetAllForceDcCurrentOverloads_HasSingleOverload()
+        {
+            var classType = typeof(CommonSteps);
+            var overloads = classType.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "ForceDcCurrent");
+
+            Assert.Single(overloads);
+        }
+
+        [Fact]
+        public void GetForceDcVoltageWithParameters_HasCorrectSignature()
+        {
+            var classType = typeof(CommonSteps);
+            var parameterTypes = new[]
+            {
+                typeof(ISemiconductorModuleContext),
+                typeof(string[]),
+                typeof(double),
+                typeof(double),
+                typeof(double)
+            };
+            var method = classType.GetMethod(
+                "ForceDcVoltage",
+                BindingFlags.Public | BindingFlags.Static,
+                binder: null,
+                parameterTypes,
+                modifiers: null);
+
+            Assert.NotNull(method);
+            var parameters = method.GetParameters();
+            AssertParameter(parameters[0], "tsmContext", false);
+            AssertParameter(parameters[1], "pinsOrPinGroups", false);
+            AssertParameter(parameters[2], "voltageLevel", false);
+            AssertParameter(parameters[3], "currentLimit", false);
+            AssertDoubleParameter(parameters[4], "settlingTime", true, 0);
+            Assert.Equal(typeof(void), method.ReturnType);
+        }
+
+        [Fact]
+        public void GetAllForceDcVoltageOverloads_HasSingleOverload()
+        {
+            var classType = typeof(CommonSteps);
+            var overloads = classType.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "ForceDcVoltage");
+
+            Assert.Single(overloads);
+        }
     }
 }
