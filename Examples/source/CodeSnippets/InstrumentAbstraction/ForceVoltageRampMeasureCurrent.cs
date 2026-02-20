9645991afab5d9b0af64a41431307b8d2c34adec
Ashutosh-NI <Ashutosh.Mishra@ni.com>
2026-02-20 12:20:05 +0530

    Add ForceVoltageRampMeasureCurrent example

---
+++ Examples/source/CodeSnippets/InstrumentAbstraction/ForceVoltageRampMeasureCurrent.cs
@@ -0,0 +1,23 @@
+﻿using NationalInstruments.ModularInstruments.NIDCPower;
+using NationalInstruments.SemiconductorTestLibrary.Common;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCPower;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+
+namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.InstrumentAbstraction
+{
+    internal static class ForceVoltageRampMeasureCurrent
+    {
+        internal static void ForceVoltageRampMeasureCurrentExample(ISemiconductorModuleContext tsmContext, string[] smuPinNames)
+        {
+            var sessionManager = new TSMSessionManager(tsmContext);
+            var voltageSequence = HelperMethods.CreateRampSequence(outputStart: 0, outputStop: 3, numberOfPoints: 10);
+            var dcPowerPins = sessionManager.DCPower(smuPinNames);
+
+            dcPowerPins.ConfigureMeasureSettings(new DCPowerMeasureSettings() { MeasureWhen = DCPowerMeasurementWhen.AutomaticallyAfterSourceComplete });
+
+            dcPowerPins.ForceVoltageSequence(voltageSequence, waitForSequenceCompletion: true, sequenceTimeoutInSeconds: 20);
+            dcPowerPins.MeasureCurrent();
+        }
+    }
+}
