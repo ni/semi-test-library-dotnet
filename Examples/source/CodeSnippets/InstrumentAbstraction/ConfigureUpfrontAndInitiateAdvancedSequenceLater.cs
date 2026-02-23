1f846de3cd7bb4edabce3e563b1bb92749ccadb2
Ashutosh.Mishra <Ashutosh.Mishra@ni.com>
2026-02-23 19:35:22 +0530

    Document example classes and update aperture times

--- Examples/source/CodeSnippets/InstrumentAbstraction/ConfigureUpfrontAndInitiateAdvancedSequenceLater.cs
+++ Examples/source/CodeSnippets/InstrumentAbstraction/ConfigureUpfrontAndInitiateAdvancedSequenceLater.cs
@@ -6,6 +6,15 @@ using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
 
 namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.InstrumentAbstraction
 {
+    /// <summary>
+    /// This class contains examples of how to use the Instrument Abstraction extensions from the Semiconductor Test Library.
+    /// Specifically, how to measure current for pins mapped to DCPower Instruments.
+    /// Note that DCPower Instruments include both Source Measurement Units (SMUs) and Programmable Power Supplies (PPS) devices.
+    /// This class, and it's methods are intended for example purposes only and are not meant to be ran standalone.
+    /// They are only meant to demonstrate specific coding concepts and may otherwise assume a hypothetical test program
+    /// with any dependent instrument sessions have been already initiated and configured.
+    /// Additionally, they are intentionally marked as internal to prevent them from being directly invoked from code outside of this project.
+    /// </summary>
     internal static class ConfigureUpfrontAndInitiateAdvancedSequenceLater
     {
         internal static void ConfigureUpfrontAndInitiateAdvancedSequenceLaterExample(ISemiconductorModuleContext tsmContext, string[] smuPinNames)
@@ -20,9 +29,9 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.Ins
             var advanceSequenceName = "MyAdvancedSequence";
             var advanceSequenceSettings = new List<DCPowerAdvancedSequenceStepProperties>
             {
-                [0] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 1.0, ApertureTime = 0.016, OutputFunction = DCPowerSourceOutputFunction.DCVoltage },
-                [1] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 2.0, ApertureTime = 0.016, OutputFunction = DCPowerSourceOutputFunction.DCVoltage },
-                [2] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 3.0, ApertureTime = 0.016, OutputFunction = DCPowerSourceOutputFunction.DCVoltage }
+                [0] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 1.0, ApertureTime = 2.0, OutputFunction = DCPowerSourceOutputFunction.DCVoltage },
+                [1] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 2.0, ApertureTime = 2.1, OutputFunction = DCPowerSourceOutputFunction.DCVoltage },
+                [2] = new DCPowerAdvancedSequenceStepProperties { VoltageLevel = 3.0, ApertureTime = 2.2, OutputFunction = DCPowerSourceOutputFunction.DCVoltage }
             };
 
             dcPowerPins.ConfigureAdvancedSequence(advanceSequenceName, advanceSequenceSettings, setAsActiveSequence: false);
@@ -55,6 +64,7 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.Ins
 
             // configure another advanced sequence with different settings
             var secondAdvanceSequence = "MyAdvancedSequence2";
+
             var voltages = new double[] { 0, 2, 3, 4 };
             var apertureTimes = new double[] { 2, 3.1, 4.3, 2.4 };
             var advanceSequenceSettingsForSecondAdvancedSequences = new DCPowerAdvancedSequenceStepProperties[4];
--- Examples/source/CodeSnippets/InstrumentAbstraction/ForceVoltageRampMeasureCurrent.cs
+++ Examples/source/CodeSnippets/InstrumentAbstraction/ForceVoltageRampMeasureCurrent.cs
@@ -6,6 +6,15 @@ using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
 
 namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.InstrumentAbstraction
 {
+    /// <summary>
+    /// This class contains examples of how to use the Instrument Abstraction extensions from the Semiconductor Test Library.
+    /// Specifically, how to measure current for pins mapped to DCPower Instruments.
+    /// Note that DCPower Instruments include both Source Measurement Units (SMUs) and Programmable Power Supplies (PPS) devices.
+    /// This class, and it's methods are intended for example purposes only and are not meant to be ran standalone.
+    /// They are only meant to demonstrate specific coding concepts and may otherwise assume a hypothetical test program
+    /// with any dependent instrument sessions have been already initiated and configured.
+    /// Additionally, they are intentionally marked as internal to prevent them from being directly invoked from code outside of this project.
+    /// </summary>
     internal static class ForceVoltageRampMeasureCurrent
     {
         internal static void ForceVoltageRampMeasureCurrentExample(ISemiconductorModuleContext tsmContext, string[] smuPinNames)
