26168af7f381fa3df2e63cc5f7e05430aa910015
mattjet27 <11801997+Mattjet27@users.noreply.github.com>
2026-03-04 12:43:39 +0100

    Update RSeries7822R class implementation and RSeries7822RAPI signatures - Rename WriteChannelData in the RSeries7822R.cs to WritePortData - Implement WritePortData such that it writes to all ports based on cached channel states that are set via a new SetOutputState method - Rename MeasureChannelData in the RSeries7822R.cs to ReadPortData - Implement ReadPortData such that it read data from a specific port - Update RSeries7822R class to internally track channel information, port configurations, and output port states - Update documentation in the RSeries7822R class - Update signatures in the RSeries7822RAPI.cs file - Add new RSeries7822RAPIException class for the ValidateStatus method to use - Add new Common/Utilities.cs file for new UpdateBitInByte helper method (will be used by TestStep.cs) - Add new RSeries7822RCustomInstrument/Types.cs file for new ChannelInfo and PortConfiguration types

--- Examples/source/Sequence/CustomInstrument.RSeries/Code Modules/RSeries7822RCustomInstrument/RSeries7822R.cs
+++ Examples/source/Sequence/CustomInstrument.RSeries/Code Modules/RSeries7822RCustomInstrument/RSeries7822R.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.CustomInstrument;
+using static NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument.RSeries.Common.Utilities;
+using static NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument.RSeries.RSeries7822RCustomInstrument.Types;
 
 namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument.RSeries.RSeries7822RCustomInstrument
 {
     /// <summary>
-    /// Concrete implementation of ICustomInstrument Interface.
+    /// Concrete implementation of ICustomInstrument Interface for controlling a PXIe-7822R R series device.
     /// </summary>
     public class RSeries7822R : ICustomInstrument
     {
@@ -12,7 +17,7 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         private int _status;
 
         /// <summary>
-        /// Instrument name that matches the one defined in the Pinmap file.
+        /// Instrument name that matches the one defined in the pin map file.
         /// </summary>
         public string InstrumentName { get; }
 
@@ -38,7 +43,28 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         public string ResourceName { get; }
 
         /// <summary>
-        /// Opens FPGA reference of the RIO instrument and stores it in '_referenceId'.
+        /// The channel information mapping.
+        /// </summary>
+        /// <remarks>
+        /// Maps an individual channel string, from the pin map definition, to it's ConnectorNumber, PortNumber, and ChannelNumber.
+        /// </remarks>
+        internal Dictionary<string, ChannelInfo> ChannelInfoMap { get; }
+
+        /// <summary>
+        /// The internal tracked states of all ports configured as output ports.
+        /// </summary>
+        /// <remarks>
+        /// Store the current state of each port. This information is needed to perform driver operation on specific port.
+        /// </remarks>
+        internal Dictionary<ChannelInfo, byte> OutputPortStates { get; private set; }
+
+        /// <summary>
+        /// The configuration of each port of the device, which can either be set as an Output or an Input.
+        /// </summary>
+        internal Dictionary<(int, int), PortConfiguration> PortConfigurations { get; private set; }
+
+        /// <summary>
+        /// Opens FPGA reference of the R series device.
         /// </summary>
         /// <param name="instrumentName">Instrument Name as defined in the Pin Map</param>
         /// <param name="channelGroupId">Channel Group Id as defined in the Pin Map</param>
@@ -50,6 +76,41 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
             ChannelGroupId = channelGroupId;
             ChannelList = channelList;
             ResourceName = InstrumentName;
+            // Organize internal tracking information of each channel based on port and connector.
+            // This will be used for easy lookup within class methods.
+            ChannelInfoMap = new Dictionary<string, ChannelInfo>();
+            OutputPortStates = new Dictionary<ChannelInfo, byte>();
+            PortConfigurations = new Dictionary<(int, int), PortConfiguration>();
+            string[] channels = ChannelList.Split(',');
+            for (int i = 0; i < channels.Length; i++)
+            {
+                var channelInfoString = channels[i];
+                var channelInfoSegments = channels[i].Split('_');
+                var channelString = channelInfoSegments.FirstOrDefault(s => s.StartsWith("DIO", StringComparison.Ordinal));
+                int channelNumber = int.Parse(
+                    channelString.Substring(channelString.LastIndexOf(channelString, StringComparison.Ordinal) + 3),
+                    NumberStyles.Integer,
+                    NumberFormatInfo.InvariantInfo);
+                var connectorString = channelInfoSegments.FirstOrDefault(s => s.StartsWith("Connector", StringComparison.Ordinal));
+                int connectorNumber = int.Parse(
+                    connectorString.Substring(connectorString.LastIndexOf(connectorString, StringComparison.Ordinal) + 3),
+                    NumberStyles.Integer,
+                    NumberFormatInfo.InvariantInfo);
+                // Ports are 8 bits wide
+                int portNumber = channelNumber / 8;
+                // This is the specific bit in port that the channel maps to.
+                int portIndex = channelNumber % 8;
+
+                ChannelInfoMap.Add(channelInfoString, new ChannelInfo(connectorNumber, portNumber, channelNumber, portIndex));
+                // Statically set port configurations such that the first two ports of each connector are designated Output ports,
+                // while the second two ports of each connector are designated Input ports.
+                // This could otherwise be exposed as a configuration method for this class but is outside of the scope of the example.
+                bool portAlreadyConfigured = PortConfigurations.TryGetValue((connectorNumber, portNumber), out var portConfiguration);
+                if (!portAlreadyConfigured)
+                {
+                    portConfiguration = (portNumber < 2) ? PortConfiguration.Output : PortConfiguration.Input;
+                }
+            }
 
             // Open FPGA reference by deploying BitFile on the RIO device of the given Instrument.
             string bitFilePath = RSeries7822RDriverAPI.BitFilePath();
@@ -59,8 +120,9 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         }
 
         /// <summary>
-        /// Closes FPGA reference of the RIO instrument.
+        /// Closes FPGA reference of the R series device.
         /// </summary>
+        /// <exception cref="Exception">Thrown when the low-level FPGA method 'CloseFPGA' fails.</exception>
         public void Close()
         {
             _status = RSeries7822RDriverAPI.CloseFPGA(_referenceId);
@@ -68,7 +130,7 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         }
 
         /// <summary>
-        /// Resets the RIO instrument.
+        /// Resets the R series device.
         /// </summary>
         public void Reset()
         {
@@ -79,7 +141,7 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         /// Enables LoopBack mode.
         /// </summary>
         /// <param name="enable">Whether to enable or disable LoopBack mode.</param>
-        /// <exception cref="Exception">Thrown when 'EnableLoopBack' fails. </exception>
+        /// <exception cref="Exception">Thrown when the low-level FPGA method 'EnableLoopBack' fails. </exception>
         public void EnableLoopBack(bool enable)
         {
             ulong value = enable ? 1UL : 0UL;
@@ -88,35 +150,71 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CustomInstrument
         }
 
         /// <summary>
-        /// RSeries card write channel data operation.
+        /// Writes data to all ports of the R series device.
         /// </summary>
-        /// <param name="channelString">Channel name.</param>
-        /// <param name="pinSiteSpecificData">Channel data to write.</param>
-        /// <exception cref="Exception">Thrown when FPGA 'WriteData' fails.</exception>
-        public void WriteChannelData(string channelString, double pinSiteSpecificData)
+        /// <exception cref="Exception">Thrown when the low-level FPGA method 'WriteData' fails.</exception>
+        public void WritePortData()
         {
-            _status = RSeries7822RDriverAPI.WriteData(_referenceId, channelString, (byte)pinSiteSpecificData);
-            ValidateStatus($"Error in WriteData method, ErrorCode:{_status}, Channel Name:{channelString}, Channel Data:{pinSiteSpecificData}");
+            foreach (var portState in OutputPortStates)
+            {
+                _status = RSeries7822RDriverAPI.WriteData(_referenceId, (uint)portState.Key.PortNumber, portState.Value);
+                ValidateStatus($"Error in WriteData method, ErrorCode:{_status}, PortNumber:{portState.Key}, PortData:{portState.Value}");
+            }
+        }
+
+        /// <summary>
+        /// Sets the state of a specific channel.
+        /// The state will be applied only when the WritePortData method is called.
+        /// </summary>
+        /// <param name="channel">The selected channel.</param>
+        /// <param name="value">Value to set.</param>
+        /// <exception cref="Exception">Thrown when the low-level FPGA method 'WriteData' fails.</exception>
+        public void SetOutputState(string channel, bool value)
+        {
+            var channelInfo = ChannelInfoMap[channel];
+            byte state;
+            if (!OutputPortStates.TryGetValue(channelInfo, out state))
+            {
+                // Initialize port state
+                state = 0;
+            }
+            byte newState = UpdateBitInByte(state, value,  channelInfo.PortIndex);
+
+            OutputPortStates[channelInfo] = state;
         }
 
         /// <summary>
         /// RSeries card read channel data operation.
         /// </summary>
-        /// <param name="channelString">Channel name.</param>
-        /// <returns>Channel data.</returns>
+        /// <returns>Port data, where the first dimension represents the connector and the second dimension represents the ports.</returns>
         /// <exception cref="Exception">Thrown when FPGA 'ReadData' fails.</exception>
-        public double MeasureChannelData(string channelString)
+        public byte[][] ReadPortData()
         {
-            _status = RSeries7822RDriverAPI.ReadData(_referenceId, channelString, out byte data);
-            ValidateStatus($"Error in ReadData method, ErrorCode:{_status}, Channel name:{channelString}");
-            return data;
+            var numberOfConnectors = ChannelInfoMap.Values.Select(x => x.ConnectorNumber).Distinct().Count();
+            var portsData = new byte[numberOfConnectors][];
+
+            var inputPorts = PortConfigurations
+                .Where(kvp => kvp.Value == PortConfiguration.Input)
+                .Select(kvp => kvp.Key)
+                .ToList();
+
+            for (int i = 0; i < inputPorts.Count; i++)
+            {
+                var key = PortConfigurations.ElementAt(i).Key;
+                int connectorNumber = key.Item2;
+                int portNumber = key.Item2;
+                _status = RSeries7822RDriverAPI.ReadData(_referenceId, (uint)portNumber, out byte data);
+                ValidateStatus($"Error in ReadData method, ErrorCode:{_status}, PortNumber:{portNumber}");
+                portsData[connectorNumber][portNumber] = data;
+            }
+            return portsData;
         }
 
         private void ValidateStatus(string exceptionMessage)
         {
             if (_status != 0)
             {
-                throw new Exception(exceptionMessage);
+                throw new RSeries7822RDriverAPIException(exceptionMessage);
             }
         }
     }
