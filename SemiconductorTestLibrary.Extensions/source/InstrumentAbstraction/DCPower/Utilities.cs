f187615e991dd668374f9187b3984874902d47aa
gmrsekar <sekar.ganesan@emerson.com>
2026-03-06 11:23:01 +0530

    Update Utilities.cs

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Utilities.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Utilities.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using NationalInstruments.ModularInstruments.NIDCPower;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCPower
@@ -13,75 +10,6 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCP
     /// </summary>
     public static class Utilities
     {
-        private static readonly Dictionary<TriggerType, IList<string>> _triggerTypeToUnsupportedModelStringMap = new Dictionary<TriggerType, IList<string>>
-        {
-            {
-                TriggerType.MeasureTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXI_4130
-                }
-            },
-            {
-                TriggerType.PulseTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXIe_4112,
-                    DCPowerModelStrings.PXIe_4113,
-                    DCPowerModelStrings.PXI_4130,
-                    DCPowerModelStrings.PXIe_4141,
-                    DCPowerModelStrings.PXIe_4143,
-                    DCPowerModelStrings.PXIe_4145,
-                    DCPowerModelStrings.PXIe_4147,
-                    DCPowerModelStrings.PXIe_4154,
-                    DCPowerModelStrings.PXIe_4162,
-                    DCPowerModelStrings.PXIe_4163,
-                    DCPowerModelStrings.PXIe_4190
-                }
-            },
-            {
-                TriggerType.SequenceAdvanceTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXI_4130
-                }
-            },
-            {
-                TriggerType.SourceTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXI_4130
-                }
-            },
-            {
-                TriggerType.StartTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXI_4130,
-                    DCPowerModelStrings.PXIe_4145
-                }
-            },
-            {
-                TriggerType.ShutdownTrigger, new List<string>
-                {
-                    DCPowerModelStrings.PXI_4110,
-                    DCPowerModelStrings.PXIe_4112,
-                    DCPowerModelStrings.PXIe_4113,
-                    DCPowerModelStrings.PXI_4130,
-                    DCPowerModelStrings.PXIe_4135,
-                    DCPowerModelStrings.PXIe_4137,
-                    DCPowerModelStrings.PXIe_4141,
-                    DCPowerModelStrings.PXIe_4143,
-                    DCPowerModelStrings.PXIe_4145,
-                    DCPowerModelStrings.PXIe_4147,
-                    DCPowerModelStrings.PXIe_4154,
-                    DCPowerModelStrings.PXIe_4162,
-                    DCPowerModelStrings.PXIe_4163,
-                    DCPowerModelStrings.PXIe_4190
-                }
-            }
-        };
-
         /// <summary>
         /// Caches mappings between properties of DCPowerAdvancedSequenceStepProperties and their corresponding DCPowerAdvancedSequenceProperty enum values.
         /// </summary>
@@ -105,17 +33,6 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCP
         internal static string ExcludeSpecificChannel(this string channelString, string channelToExclude)
         {
             return string.Join(",", channelString.Split(',').Where(s => !s.Contains($"/{channelToExclude}")));
-        }
-
-        internal static IEnumerable<TriggerType> GetUnsupportedTriggerTypes(string modelString)
-        {
-            foreach (TriggerType triggerType in _triggerTypeToUnsupportedModelStringMap.Keys)
-            {
-                if (_triggerTypeToUnsupportedModelStringMap[triggerType].Contains(modelString))
-                {
-                    yield return triggerType;
-                }
-            }
         }
     }
 }
