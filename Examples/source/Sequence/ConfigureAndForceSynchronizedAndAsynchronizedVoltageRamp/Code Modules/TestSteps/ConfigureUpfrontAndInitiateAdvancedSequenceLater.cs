3ccbaebcca7e99d82eef292f6bdec4822edab8e3
Ashutosh.Mishra <Ashutosh.Mishra@ni.com>
2026-03-25 17:29:41 +0530

    Add InitiateAdvancedSequence and update project files

--- Examples/source/Sequence/ConfigureAndForceSynchronizedAndAsynchronizedVoltageRamp/Code Modules/TestSteps/ConfigureUpfrontAndInitiateAdvancedSequenceLater.cs
+++ Examples/source/Sequence/ConfigureAndForceSynchronizedAndAsynchronizedVoltageRamp/Code Modules/TestSteps/ConfigureUpfrontAndInitiateAdvancedSequenceLater.cs
@@ -12,7 +12,7 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.Ins
     public static partial class TestSteps
     {
         /// <summary>
-        /// This method demonstrates how to configure an advanced sequence upfront without setting it as the active sequence, allowing you to initiate it later in your test flow without needing to reconfigure it. This is useful in scenarios.
+        /// This method demonstrates how to configure an advanced sequence upfront without setting it as the active sequence, allowing you to initiate it later in your test flow without needing to reconfigure it.
         /// </summary>
         /// <remarks>
         /// This method is not supported by all instruments. Refer to the <a href="https://www.ni.com/docs/en-US/bundle/ni-dcpower-c-api-ref/page/group____root__nidcpower__supported__functions__by__device.html">Supported Functions by Device</a> topic in the NI DC Power Supplies and SMUs Help for information about supported instruments.
@@ -42,7 +42,18 @@ namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.Ins
             };
 
             dcPowerPins.ConfigureAdvancedSequence(advanceSequenceName, advanceSequenceSettings, setAsActiveSequence: false);
+        }
 
+        /// <summary>
+        /// This method demonstrates initiating a previously configured advanced sequence.
+        /// </summary>
+        /// <param name="tsmContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
+        /// <param name="smuPinNames">The SMU pins to initiate the advanced sequence on.</param>
+        /// <param name="advanceSequenceName">The name of the advanced sequence to initiate.(advanced sequence name should be same as what was created in <see cref="ConfigureUpfrontAndInitiateAdvancedSequenceLater"/>).</param>
+        public static void InitiateAdvancedSequence(ISemiconductorModuleContext tsmContext, string[] smuPinNames, string advanceSequenceName)
+        {
+            var sessionManager = new TSMSessionManager(tsmContext);
+            var dcPowerPins = sessionManager.DCPower(smuPinNames);
             // Initiate the advanced sequence that was configured earlier
             dcPowerPins.InitiateAdvancedSequence(advanceSequenceName);
         }
