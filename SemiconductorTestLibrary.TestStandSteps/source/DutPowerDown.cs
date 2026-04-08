02da30aed0ab652965a0166925e187cf53330326
Ashutosh-NI <Ashutosh.Mishra@ni.com>
2026-04-08 03:06:33 -0500

    [BugFix] Refactor DutPowerDown and integrate power-supply settings (#461)

--- SemiconductorTestLibrary.TestStandSteps/source/DutPowerDown.cs
+++ SemiconductorTestLibrary.TestStandSteps/source/DutPowerDown.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using NationalInstruments.SemiconductorTestLibrary.Common;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
 using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
@@ -84,15 +86,30 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
         {
             var originalSourceDelays = dcPower.GetSourceDelayInSeconds();
             dcPower.ConfigureSourceDelay(settlingTimeInSeconds);
+            var containsPinMappedToPowerSupply = dcPower.ContainsPinMappedToPowerSupplyInstrument(forceLowestCurrentLimit, out var dcPowerSourceSettings);
             if (forceLowestCurrentLimit)
             {
                 var originalCurrentLimit = dcPower.GetCurrentLimits();
-                dcPower.ForceVoltage(voltageLevel: 0, currentLimit: 1e-7);
+                if (containsPinMappedToPowerSupply)
+                {
+                    dcPower.ForceVoltage(dcPowerSourceSettings);
+                }
+                else
+                {
+                    dcPower.ForceVoltage(voltageLevel: 0, currentLimit: 1e-7);
+                }
                 dcPower.ConfigureCurrentLimit(originalCurrentLimit);
             }
             else
             {
-                dcPower.ForceVoltage(voltageLevel: 0);
+                if (containsPinMappedToPowerSupply)
+                {
+                    dcPower.ForceVoltage(dcPowerSourceSettings);
+                }
+                else
+                {
+                    dcPower.ForceVoltage(voltageLevel: 0);
+                }
             }
             dcPower.ConfigureSourceDelay(originalSourceDelays);
         }
@@ -106,5 +123,58 @@ namespace NationalInstruments.SemiconductorTestLibrary.TestStandSteps
                 channelOutput.ConfigureCurrentLimit(currentLimits.GetValue(sitePinInfo.SiteNumber, sitePinInfo.PinName));
             });
         }
+
+        private static bool ContainsPinMappedToPowerSupplyInstrument(this DCPowerSessionsBundle dcPower, bool forceLowestCurrentLimit, out PinSiteData<DCPowerSourceSettings> dcPowerSourceSettings)
+        {
+            bool containsPinMappedToPowerSupply = false;
+            dcPowerSourceSettings = dcPower.DoAndReturnPerSitePerPinResults((sessionInfo, sitePinInfo) =>
+            {
+                var output = sessionInfo.Session.Outputs[sitePinInfo.IndividualChannelString];
+                bool isOverRangingEnabled = output.Source.OverrangingEnabled;
+                bool isPowerSupply = PowerSupplySettings.TryGetValue(sitePinInfo.ModelString, out var powerSupplyInstrumentConfiguration);
+                if (isPowerSupply)
+                {
+                    containsPinMappedToPowerSupply = true;
+                }
+
+                return new DCPowerSourceSettings
+                {
+                    Level = isPowerSupply ? powerSupplyInstrumentConfiguration.GetVoltageLevel(isOverRangingEnabled) : 0.00,
+                    Limit = forceLowestCurrentLimit ? (isPowerSupply ? powerSupplyInstrumentConfiguration.GetCurrentLimit(isOverRangingEnabled) : 1e-7) : (double?)null,
+                };
+            });
+
+            return containsPinMappedToPowerSupply;
+        }
+
+        internal sealed class PowerSupplyInstrumentConfiguration
+        {
+            private readonly double _overRangingEnabledVoltageLevel;
+            private readonly double _overRangingEnabledCurrentLimit;
+            private readonly double _overRangingDisabledVoltageLevel;
+            private readonly double _overRangingDisabledCurrentLimit;
+
+            public PowerSupplyInstrumentConfiguration(double overRangingEnabledVoltageLevel, double overRangingEnabledCurrentLimit, double overRangingDisabledVoltageLevel, double overRangingDisabledCurrentLimit)
+            {
+                _overRangingEnabledVoltageLevel = overRangingEnabledVoltageLevel;
+                _overRangingEnabledCurrentLimit = overRangingEnabledCurrentLimit;
+                _overRangingDisabledVoltageLevel = overRangingDisabledVoltageLevel;
+                _overRangingDisabledCurrentLimit = overRangingDisabledCurrentLimit;
+            }
+            public double GetVoltageLevel(bool isOverRangingEnabled) => isOverRangingEnabled ? _overRangingEnabledVoltageLevel : _overRangingDisabledVoltageLevel;
+            public double GetCurrentLimit(bool isOverRangingEnabled) => isOverRangingEnabled ? _overRangingEnabledCurrentLimit : _overRangingDisabledCurrentLimit;
+        }
+
+        internal static readonly IReadOnlyDictionary<string, PowerSupplyInstrumentConfiguration> PowerSupplySettings =
+            new Dictionary<string, PowerSupplyInstrumentConfiguration>
+            {
+                [DCPowerModelStrings.PXI_4110] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.0, overRangingEnabledCurrentLimit: 0.01, overRangingDisabledVoltageLevel: 0.0, overRangingDisabledCurrentLimit: 0.01),
+                [DCPowerModelStrings.PXIe_4112] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.07, overRangingEnabledCurrentLimit: 0.007, overRangingDisabledVoltageLevel: 0.1, overRangingDisabledCurrentLimit: 0.01),
+                [DCPowerModelStrings.PXIe_4113] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.025, overRangingEnabledCurrentLimit: 0.015, overRangingDisabledVoltageLevel: 0.03, overRangingDisabledCurrentLimit: 0.02),
+                [DCPowerModelStrings.PXIe_4051] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.0, overRangingEnabledCurrentLimit: 0.4, overRangingDisabledVoltageLevel: 0.0, overRangingDisabledCurrentLimit: 0.4),
+                [DCPowerModelStrings.PXIe_4150] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.01, overRangingEnabledCurrentLimit: 0.15, overRangingDisabledVoltageLevel: 0.01, overRangingDisabledCurrentLimit: 0.15),
+                [DCPowerModelStrings.PXIe_4151] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.01, overRangingEnabledCurrentLimit: 0.3, overRangingDisabledVoltageLevel: 0.01, overRangingDisabledCurrentLimit: 0.3),
+                [DCPowerModelStrings.PXIe_4154] = new PowerSupplyInstrumentConfiguration(overRangingEnabledVoltageLevel: 0.0, overRangingEnabledCurrentLimit: 0.01, overRangingDisabledVoltageLevel: 0.0, overRangingDisabledCurrentLimit: 0.01),
+            };
     }
 }
