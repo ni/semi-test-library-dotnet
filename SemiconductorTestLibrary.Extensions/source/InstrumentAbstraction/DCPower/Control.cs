250a1357f594b29d44f5552d2c7eb2bcfc197c0a
Ashutosh.Mishra <Ashutosh.Mishra@ni.com>
2026-04-09 11:52:22 +0530

    Enable sequence mode and wait for sequence completion

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Control.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Control.cs
@@ -1,4 +1,5 @@
-﻿using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
+﻿using NationalInstruments.ModularInstruments.NIDCPower;
+using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCPower
 {
@@ -79,11 +80,12 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCP
             {
                 var allChannelOutput = sessionInfo.AllChannelsOutput;
                 allChannelOutput.Control.Abort();
+                allChannelOutput.Source.Mode = DCPowerSourceMode.Sequence;
                 allChannelOutput.Source.AdvancedSequencing.ActiveAdvancedSequence = sequenceName;
                 allChannelOutput.Control.Initiate();
                 if (waitForSequenceCompletion)
                 {
-                    allChannelOutput.Events.SourceCompleteEvent.WaitForEvent(PrecisionTimeSpan.FromSeconds(sequenceTimeoutInSeconds));
+                    allChannelOutput.Events.SequenceEngineDoneEvent.WaitForEvent(PrecisionTimeSpan.FromSeconds(sequenceTimeoutInSeconds));
                 }
             });
         }
