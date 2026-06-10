3a39535446ae4b58fd567b34e9c7cceb768d5430
Shalini-Subramanian <shalini.subramanian@ni.com>
2026-06-10 10:55:28 +0530

    fix: Update pin names
    Signed-off-by: Shalini-Subramanian <shalini.subramanian@ni.com>

--- SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/DCPower/SourceTests.cs
+++ SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/DCPower/SourceTests.cs
@@ -5105,7 +5105,7 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbst
         public void DifferentSMUDevices_ConfigureCurrentLimitLowWithScalarValues_CorrectCurrentLimitLowSet(string pinMap)
         {
             var sessionManager = Initialize(pinMap);
-            var sessionsBundle = sessionManager.DCPower("VCC1");
+            var sessionsBundle = sessionManager.DCPower("VCC2");
             var expectedCurrentLimitLow = -1E-3;
             sessionsBundle.Do((sessionInfo, sitePinInfo) =>
             {
@@ -5150,7 +5150,7 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbst
         public void DifferentSMUDevices_ConfigureCurrentLimitLowWithPerSiteValues_CorrectCurrentLimitLowSet(string pinMap)
         {
             var sessionManager = Initialize(pinMap);
-            var sessionsBundle = sessionManager.DCPower("VCC1");
+            var sessionsBundle = sessionManager.DCPower("VCC2");
             var currentLimitLow = new SiteData<double>(new[] { -1E-2, -1E-3 });
             sessionsBundle.Do((sessionInfo, sitePinInfo) =>
             {
