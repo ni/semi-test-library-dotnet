013e3720a00d9a5fc69d7ed4cc739060c41536eb
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2026-06-16 23:19:27 +0530

    Adding clock related extensions

---
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/FGen/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.FGen
+{
+    /// <summary>
+    /// Container for Clock extension methods
+    /// </summary>
+    public static class Clock
+    {
+        /// <summary>
+        /// Configure Clock Mode
+        /// </summary>
+        public static void ConfigureClockMode()
+        { }
+
+        /// <summary>
+        /// Configure Sample Source
+        /// </summary>
+        public static void ConfigureSampleSource()
+        { }
+
+        /// <summary>
+        /// Configure Reference Clock
+        /// </summary>
+        public static void ConfigureReferenceClock()
+        { }
+    }
+}
