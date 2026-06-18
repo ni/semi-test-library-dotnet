1d7a1f7c67b6ffdb15336eff0ad9c0d06ec89d14
தே.க.தமிழ் <anish.t@emerson.com>
2026-06-18 19:36:53 +0530

    Updated timing

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/Scope/Timing.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/Scope/Timing.cs
@@ -1,6 +1,4 @@
-﻿using NationalInstruments.ModularInstruments.NIScope;
-
-using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
+﻿using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Scope
 {
@@ -13,16 +11,12 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Sco
         /// Configures the timing parameters for the acquisition.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
-        /// <param name="sampleRateMin">The minimum sample rate in samples per second.</param>
-        /// <param name="recordLengthMin">The minimum record length in samples.</param>
-        /// <param name="referencePosition">The reference position as a percentage of the record length.</param>
-        /// <param name="numberOfRecords">The number of records to acquire.</param>
-        /// <param name="enforceRealTime">Whether to enforce real-time acquisition.</param>
-        public static void ConfigureTiming(this ScopeSessionsBundle sessionsBundle, double sampleRateMin, int recordLengthMin, double referencePosition, int numberOfRecords, bool enforceRealTime)
+        /// <param name="timingSettings">The timing settings for the acquisition.</param>
+        public static void ConfigureTiming(this ScopeSessionsBundle sessionsBundle, ScopeTimingSettings timingSettings)
         {
             sessionsBundle.Do(sessionInfo =>
             {
-                sessionInfo.Session.Timing.ConfigureTiming(sampleRateMin, recordLengthMin, referencePosition, numberOfRecords, enforceRealTime);
+                sessionInfo.Session.Timing.ConfigureTiming(timingSettings.SampleRateMin.Value, timingSettings.RecordLengthMin.Value, timingSettings.ReferencePosition.Value, timingSettings.NumberOfRecords.Value, timingSettings.EnforceRealTime.Value);
             });
         }
     }
