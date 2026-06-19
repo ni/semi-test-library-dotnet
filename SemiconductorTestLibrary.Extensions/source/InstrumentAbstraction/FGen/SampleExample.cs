6e04ba5d3fdea2f2539c033c874587d9d1882746
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2026-06-19 21:01:07 +0530

    Updated example comments

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/FGen/SampleExample.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/FGen/SampleExample.cs
@@ -2,7 +2,10 @@
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.FGen
 {
-    internal class SampleExample
+    /// <summary>
+    /// Sample example class to illustrate how the extensions are used in a typical waveform generation.
+    /// </summary>
+    public static class SampleExample
     {
         /// <summary>
         /// Example method to show generation of sinewaveform
@@ -15,6 +18,9 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.FGe
         public static void GenerateSineWaveform(StandardFunction function, double frequency, double amplitude, double dcOffest = 0, double startPhase = 0)
         {
             /*
+             * Create TSM Session manager
+             * Create FGen Sessions Bundle
+             *
              * ConfigureOutputMode
              * ConfigureChannel
              * ConfigureStandardWaveform
