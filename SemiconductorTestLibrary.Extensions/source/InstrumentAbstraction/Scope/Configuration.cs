960111753a33348d7a191d9cf0ab15c36a32dd3a
தே.க.தமிழ் <anish.t@emerson.com>
2026-06-23 23:11:35 +0530

    Renamed to be more meaningful

---
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/Scope/Configuration.cs
@@ -0,0 +1,92 @@
+using NationalInstruments.ModularInstruments.NIScope;
+using NationalInstruments.SemiconductorTestLibrary.Common;
+using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
+
+using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
+
+namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Scope
+{
+    /// <summary>
+    /// Defines methods for configuring oscilloscope instruments.
+    /// </summary>
+    public static class Configuration
+    {
+        /// <summary>
+        /// Configures the vertical settings of the specified channel.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
+        /// <param name="settings">The channel settings object.</param>
+        public static void ConfigureChannel(this ScopeSessionsBundle sessionsBundle, ScopeChannelSettings settings)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureChannel(settings, sitePinInfo);
+            });
+        }
+
+        /// <inheritdoc cref="ConfigureChannel(ScopeSessionsBundle, ScopeChannelSettings)"/>
+        public static void ConfigureChannel(this ScopeSessionsBundle sessionsBundle, SiteData<ScopeChannelSettings> settings)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureChannel(settings.GetValue(sitePinInfo.SiteNumber), sitePinInfo);
+            });
+        }
+
+        /// <inheritdoc cref="ConfigureChannel(ScopeSessionsBundle, ScopeChannelSettings)"/>
+        public static void ConfigureChannelSettings(this ScopeSessionsBundle sessionsBundle, PinSiteData<ScopeChannelSettings> settings)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureChannel(settings.GetValue(sitePinInfo), sitePinInfo);
+            });
+        }
+
+        /// <summary>
+        /// Configures the electrical characteristics of the specified channel.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
+        /// <param name="channelCharacteristics">The electrical characteristics of the channel.</param>
+        public static void ConfigureCharacteristics(this ScopeSessionsBundle sessionsBundle, ChannelCharacteristics channelCharacteristics)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureCharacteristics(channelCharacteristics, sitePinInfo);
+            });
+        }
+
+        /// <inheritdoc cref="ConfigureCharacteristics(ScopeSessionsBundle, ChannelCharacteristics)"/>
+        public static void ConfigureCharacteristics(this ScopeSessionsBundle sessionsBundle, SiteData<ChannelCharacteristics> channelCharacteristics)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureCharacteristics(channelCharacteristics.GetValue(sitePinInfo.SiteNumber), sitePinInfo);
+            });
+        }
+
+        /// <inheritdoc cref="ConfigureCharacteristics(ScopeSessionsBundle, ChannelCharacteristics)"/>
+        public static void ConfigureCharacteristics(this ScopeSessionsBundle sessionsBundle, PinSiteData<ChannelCharacteristics> channelCharacteristics)
+        {
+            sessionsBundle.Do((sessionInfo, sitePinInfo) =>
+            {
+                sessionInfo.ConfigureCharacteristics(channelCharacteristics.GetValue(sitePinInfo), sitePinInfo);
+            });
+        }
+
+        internal static void ConfigureChannel(this ScopeSessionInformation sessionInfo, ScopeChannelSettings settings, SitePinInfo sitePinInfo)
+        {
+            var channel = sessionInfo.Session.Channels[sitePinInfo.IndividualChannelString];
+            channel.Configure(settings.Range, settings.Offset, (ScopeVerticalCoupling)settings.Coupling, settings.ProbeAttenuation, settings.Enabled);
+            if (settings.EnableTimeInterleavedSampling.HasValue)
+            {
+                channel.EnableTimeInterleavedSampling = settings.EnableTimeInterleavedSampling.Value;
+            }
+        }
+
+        internal static void ConfigureCharacteristics(this ScopeSessionInformation sessionInfo, ChannelCharacteristics channelCharacteristics, SitePinInfo sitePinInfo)
+        {
+            var channel = sessionInfo.Session.Channels[sitePinInfo.IndividualChannelString];
+            channel.ConfigureCharacteristics(channelCharacteristics.InputImpedance, channelCharacteristics.InputFrequencyMax);
+        }
+    }
+}
--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/Scope/Measurement.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/Scope/Measurement.cs
@@ -1,61 +1,70 @@
+using NationalInstruments.SemiconductorTestLibrary.Common;
+
 using static NationalInstruments.SemiconductorTestLibrary.Common.ParallelExecution;
 
 namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Scope
 {
     /// <summary>
-    /// Defines methods for performing measurements on the NI-Scope session.
+    /// Defines methods for performing oscilloscope measurements.
     /// </summary>
     public static class Measurement
     {
-        #region methods on ScopeSessionsBundle
-
         /// <summary>
-        /// Aborts an in-progress acquisition on all sessions in the bundle.
+        /// Reads waveform data from the specified channel.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
-        public static void Abort(this ScopeSessionsBundle sessionsBundle)
+        /// <param name="channelName">The name of the channel to read from.</param>
+        /// <param name="timeout">The maximum time to wait for the data.</param>
+        /// <param name="recordLength">The number of samples to read.</param>
+        /// <param name="waveforms">The waveform array to populate with the data.</param>
+        /// <returns>An array of waveforms containing the measurement data.</returns>
+        public static AnalogWaveformCollection<double>[] ReadWaveform(this ScopeSessionsBundle sessionsBundle, string channelName, PrecisionTimeSpan timeout, long recordLength, AnalogWaveformCollection<double>[] waveforms)
         {
-            sessionsBundle.Do(sessionInfo =>
+            return sessionsBundle.DoAndReturnPerInstrumentPerChannelResults(sessionInfo =>
             {
-                sessionInfo.Session.Measurement.Abort();
+                return sessionInfo.Session.Channels[channelName].Measurement.Read(timeout, recordLength, waveforms[0]);
             });
         }
 
         /// <summary>
-        /// Performs auto-setup on all sessions in the bundle.
+        /// Fetches waveform data from the specified channel without initiating a new acquisition.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
-        public static void AutoSetup(this ScopeSessionsBundle sessionsBundle)
+        /// <param name="channelName">The name of the channel to fetch from.</param>
+        /// <param name="timeout">The maximum time to wait for the data.</param>
+        /// <param name="pointsToFetch">The number of points to fetch. Use -1 to fetch all available points.</param>
+        /// <param name="waveforms">The waveform array to populate with the data.</param>
+        /// <returns>An array of waveforms containing the fetched data.</returns>
+        public static AnalogWaveformCollection<double>[] FetchWaveform(this ScopeSessionsBundle sessionsBundle, string channelName, PrecisionTimeSpan timeout, int pointsToFetch, AnalogWaveformCollection<double>[] waveforms)
         {
-            sessionsBundle.Do(sessionInfo =>
+            return sessionsBundle.DoAndReturnPerInstrumentPerChannelResults(sessionInfo =>
             {
-                sessionInfo.Session.Measurement.AutoSetup();
+                return sessionInfo.Session.Channels[channelName].Measurement.FetchDouble(timeout, pointsToFetch, waveforms[0]);
             });
         }
 
         /// <summary>
-        /// Commits the acquisition on all sessions in the bundle.
+        /// Fetches waveform data with detailed waveform information from the specified channel.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
-        public static void Commit(this ScopeSessionsBundle sessionsBundle)
-        {
-            sessionsBundle.Do(sessionInfo =>
-            {
-                sessionInfo.Session.Measurement.Commit();
-            });
-        }
-
-        /// <summary>
-        /// Starts acquisition on all sessions in the bundle.
-        /// </summary>
-        /// <param name="sessionsBundle">The <see cref="ScopeSessionsBundle"/> object.</param>
-        public static void Initiate(this ScopeSessionsBundle sessionsBundle)
-        {
-            sessionsBundle.Do(sessionInfo =>
-            {
-                sessionInfo.Session.Measurement.Initiate();
-            });
-        }
-        #endregion methods on ScopeSessionsBundle
+        /// <param name="channelName">The name of the channel to fetch from.</param>
+        /// <param name="timeout">The maximum time to wait for the data.</param>
+        /// <param name="pointsToFetchMax">The maximum number of points to fetch.</param>
+        /// <param name="waveforms">The waveform array to populate with the data.</param>
+        /// <param name="waveformInfo">Output parameter containing detailed waveform information.</param>
+        /// <returns>An array of waveforms containing the fetched data.</returns>
+        // public static AnalogWaveformCollection<double>[][] FetchWaveformWithInfo(this ScopeSessionsBundle sessionsBundle, string channelName, PrecisionTimeSpan timeout, int pointsToFetchMax, AnalogWaveformCollection <double>[] waveforms, out WaveformInfo[][] waveformInfo)
+        // {
+        //    var waveformInfoList = new List<WaveformInfo[]>();
+        //    var results = sessionsBundle.DoAndReturnPerInstrumentPerChannelResults(sessionInfo =>
+        //    {
+        //        WaveformInfo[] info;
+        //        var wfm = sessionInfo.Session.Channels[channelName].Control.FetchDouble(timeout, pointsToFetchMax, waveforms, out info);
+        //        waveformInfoList.Add(info);
+        //        return wfm;
+        //    });
+        //    waveformInfo = waveformInfoList.ToArray();
+        //    return results;
+        // }
     }
 }
