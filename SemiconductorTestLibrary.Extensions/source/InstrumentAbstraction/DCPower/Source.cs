9167b58381c7668b6d4556c9b5537cdc0e464b0a
Selvaraj, Arunkumar <arunkumar.selvaraj@emerson.com>
2026-07-15 13:36:09 +0800

    refactor: remove unwanted methods added during local merge
    Signed-off-by: Selvaraj, Arunkumar <arunkumar.selvaraj@emerson.com>

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Source.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Source.cs
@@ -2277,7 +2277,7 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCP
         /// Gets the compliance limit symmetry.
         /// </summary>
         /// <param name="sessionsBundle">The <see cref="DCPowerSessionsBundle"/> object.</param>
-        /// <returns>The per-pin per-site compliance limit symmetry.</returns>
+        /// <returns>The per-site per-pin compliance limit symmetry.</returns>
         public static PinSiteData<DCPowerComplianceLimitSymmetry> GetLimitSymmetry(this DCPowerSessionsBundle sessionsBundle)
         {
             return sessionsBundle.DoAndReturnPerSitePerPinResults((sessionInfo, sitePinInfo) =>
