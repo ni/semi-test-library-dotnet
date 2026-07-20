550185d0d9b06877857178f516c917fffeee3686
Shalini-Subramanian <shalini.subramanian@ni.com>
2026-07-20 13:04:33 +0530

    fix: Update exception in the documentation
    Signed-off-by: Shalini-Subramanian <shalini.subramanian@ni.com>

--- SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Source.cs
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/DCPower/Source.cs
@@ -2069,6 +2069,7 @@ namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.DCP
         /// When ganged pins are configured using individual pin names, all pins in the ganged group must have the same value; otherwise an exception is thrown.
         /// Otherwise, when the value is associated with individual pin names, the voltage level for each pin is set to the specified value.
         /// </remarks>
+        /// <exception cref="NISemiconductorTestException">Thrown when the ganged pins are configured using individual pin names with different values.</exception>
         public static void ConfigureVoltageLevel(this DCPowerSessionsBundle sessionsBundle, PinSiteData<double> voltageLevel)
         {
             var hasGangedChannels = sessionsBundle.HasGangedChannels;
