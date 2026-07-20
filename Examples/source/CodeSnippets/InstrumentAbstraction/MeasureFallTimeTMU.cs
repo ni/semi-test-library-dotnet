b1f3086dbd71b488070c762c52cda4b0018368a3
Saxena, Utkarsh <utkarsh.saxena@emerson.com>
2026-07-20 04:19:28 -0500

    Minor fix in Fall Time Measurement Example

---
+++ Examples/source/CodeSnippets/InstrumentAbstraction/MeasureFallTimeTMU.cs
@@ -0,0 +1,87 @@
+using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Digital;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Digital.TMU;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+
+namespace NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.InstrumentAbstraction
+{
+    /// <summary>
+    /// Provides an example method demonstrating TMU fall time measurement functionality using STL.
+    /// </summary>
+    public static class MeasureFallTimeTMU
+    {
+        /// <summary>
+        /// Demonstrates how to measure the fall time of a digital signal using the TMU.
+        /// Fall time is defined as the time for a signal to transition from the high voltage
+        /// threshold (Voh) to the low voltage threshold (Vol). This measurement requires
+        /// 2 comparators per pin.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This method performs the following steps:
+        /// <list type="number">
+        ///   <item>Queries the TSM session manager to get the digital sessions bundle associated with the "C0" pin.</item>
+        ///   <item>Assigns TMU resources to the specified pins.</item>
+        ///   <item>Configures the TMU for fall time measurement.</item>
+        ///   <item>Enables the TMU resource at the hardware level.</item>
+        ///   <item>Initiates the TMU measurement.</item>
+        ///   <item>Fetches and averages the measurement results.</item>
+        ///   <item>Cleans up by disabling the TMU and clearing assignments.</item>
+        /// </list>
+        /// </para>
+        /// <para>
+        /// Unlike <see cref="TmuExtensions.ConfigurePeriodMeasurement"/>, the
+        /// <see cref="TmuExtensions.ConfigureTMUFallTimeMeasurement"/> method does not enable the TMU.
+        /// An explicit call to <see cref="TmuExtensions.EnableTMU"/> is required after configuration
+        /// and before initiating the measurement.
+        /// </para>
+        /// <para>
+        /// Ensure that the pin map includes "C0" and that the hardware
+        /// is properly configured before calling this method.
+        /// </para>
+        /// </remarks>
+        /// <param name="tsmContext">The <see cref="ISemiconductorModuleContext"/> object.</param>
+        public static void MeasureFallTimeWithSTL(ISemiconductorModuleContext tsmContext)
+        {
+            // Configuration parameters for TMU fall time measurement.
+            int numberOfSamples = 100;           // Number of fall time samples to collect.
+            double timeoutInSeconds = 5.0;       // Maximum time to wait for measurement completion.
+
+            // Step 1: Query TSM session manager to get the digital sessions bundle associated with the "C0" pin.
+            var sessionManager = new TSMSessionManager(tsmContext);
+            var digitalPins = sessionManager.Digital("C0");
+
+            // Step 2: (Mandatory) Assign TMU resources to the digital pins.
+            // This assigns a TMU resource to each of the pins in the digital sessions bundle object,
+            // in this case just the "C0" pin.
+            // Note that the TMU hardware resource is not reserved until step 4.
+            digitalPins.AssignTMUResources();
+
+            // Step 3: Configure the TMU to perform a fall time measurement.
+            // Sets the start source to Voh on falling edge and the stop source to Vol on falling edge.
+            // - samplesToAcquire: Number of fall time measurements to collect.
+            // - armType: Start measurement immediately without waiting for an arm event.
+            // Note: This method does NOT enable (reserve) the TMU resource at the hardware level.
+            digitalPins.ConfigureTMUFallTimeMeasurement(
+                samplesToAcquire: numberOfSamples,
+                armType: TmuArmType.Immediate);
+
+            // Step 4: Enable (reserve) the TMU resource at the hardware level.
+            // This step is required when using ConfigureTMUFallTimeMeasurement.
+            digitalPins.EnableTMU();
+
+            // Step 5: Initiate the TMU measurement.
+            digitalPins.TMUInitiate();
+
+            // Step 6: Fetch the averaged measurement results.
+            // The TMU collects multiple samples and returns the average fall time.
+            PinSiteData<double> fallTimeMeasurements = digitalPins.FetchAveragedTMUMeasurement(timeoutInSeconds);
+
+            // Step 7: Clean up TMU resources.
+            // Always disable the TMU and clear assignments when finished to free up resources.
+            digitalPins.DisableTMU();
+            digitalPins.ClearTMUAssignment();
+        }
+    }
+}
