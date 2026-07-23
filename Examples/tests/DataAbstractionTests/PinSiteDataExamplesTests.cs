327bc2a558785fbe7ebfcb9618ec33ba999d2c71
gmrsekar <sekar.ganesan@emerson.com>
2026-07-23 11:49:39 +0530

    Auto tests for Examples

---
+++ Examples/tests/DataAbstractionTests/PinSiteDataExamplesTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using static NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.DataAbstraction.PinSiteDataExamples;
+
+namespace NationalInstruments.Tests.SemiconductorTestLibrary.DataAbstractionTests
+{
+    
+public class PinSiteDataExamplesTests
+    {
+        [Fact]
+        public void ConstructWithDefaultConstructor_Succeeds()
+        {
+            ConstructWithDefaultConstructor();
+        }
+
+        [Fact]
+        public void ConstructWithSinglePinNameAndSiteNumbers_Succeeds()
+        {
+            ConstructWithSinglePinNameAndSiteNumbers();
+        }
+
+        [Fact]
+        public void ConstructWithPinNamesAndSiteNumbers_Succeeds()
+        {
+            ConstructWithPinNamesAndSiteNumbers();
+        }
+    }
+}
