aafe792f5e2c143fa2e8ea8a8260eb3d2a93d2eb
Nagaraja H C <nagaraja.chandrappa@emerson.com>
2026-07-27 20:39:10 +0530

    Adding Reset and ResetDevice extension methods along with unit tests

---
+++ SemiconductorTestLibrary.Extensions/source/InstrumentAbstraction/FGen/Utilities.cs
@@ -0,0 +1,37 @@
+﻿using NationalInstruments.SemiconductorTestLibrary.Common;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Fgen;
+
+namespace NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.FGen
+{
+    /// <summary>
+    /// Defines utility methods for NI-FGen session.
+    /// </summary>
+    public static class Utilities
+    {
+        /// <summary>
+        /// Resets the instrument to a known state. This VI aborts the generation, clears all routes, and resets session properties to the default values.
+        /// This VI does not, however, commit the session properties or configure the device hardware to its default state.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="FgenSessionsBundle"/> object.</param>
+        public static void Reset(this FgenSessionsBundle sessionsBundle)
+        {
+            sessionsBundle.Do(sessionInfo =>
+            {
+                sessionInfo.Session.Utility.Reset();
+            });
+        }
+
+        /// <summary>
+        /// Performs a hard reset on the device. Generation is stopped, all routes are released, external bidirectional terminals are tri-stated,
+        /// FPGAs are reset, hardware is configured to its default state, and all session properties are reset to their default states.
+        /// </summary>
+        /// <param name="sessionsBundle">The <see cref="FgenSessionsBundle"/> object.</param>
+        public static void ResetDevice(this FgenSessionsBundle sessionsBundle)
+        {
+            sessionsBundle.Do(sessionInfo =>
+            {
+                sessionInfo.Session.Utility.ResetDevice();
+            });
+        }
+    }
+}
---
+++ SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/FGen/Utilities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.Fgen;
+using NationalInstruments.SemiconductorTestLibrary.InstrumentAbstraction.FGen;
+using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+using Xunit;
+using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+
+namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbstraction.FGen
+{
+    [Collection("NonParallelizable")]
+    public sealed class Utilities : IDisposable
+    {
+        private ISemiconductorModuleContext _tsmContext;
+
+        public TSMSessionManager Initialize(string pinMapFileName)
+        {
+            _tsmContext = CreateTSMContext(pinMapFileName);
+            InitializeAndClose.Initialize(_tsmContext);
+            return new TSMSessionManager(_tsmContext);
+        }
+
+        public void Dispose()
+        {
+            InitializeAndClose.Close(_tsmContext);
+        }
+
+        [Fact]
+        public void InitializeBundleWithSinglePin_PerformResetOperation__Succeeds()
+        {
+            var sessionManager = Initialize("Fgen.pinmap");
+            var sessionsBundle = sessionManager.Fgen("Pin1");
+            sessionsBundle.Reset();
+        }
+
+        [Fact]
+        public void InitializeBundleWithSinglePin_PerformResetDeviceOperation_Succeeds()
+        {
+            var sessionManager = Initialize("Fgen.pinmap");
+            var sessionsBundle = sessionManager.Fgen("Pin1");
+            sessionsBundle.ResetDevice();
+        }
+
+        [Fact]
+        public void InitializeBundleWithMultiplePin_PerformResetOperation_Succeeds()
+        {
+            var sessionManager = Initialize("Fgen.pinmap");
+            var sessionsBundle = sessionManager.Fgen("Pin1");
+            sessionsBundle.Reset();
+        }
+
+        [Fact]
+        public void InitializeBundleWithMultiplePin_PerformResetDeviceOperation_Succeeds()
+        {
+            var sessionManager = Initialize("Fgen.pinmap");
+            var sessionsBundle = sessionManager.Fgen("Pin1");
+            sessionsBundle.ResetDevice();
+        }
+    }
+}
