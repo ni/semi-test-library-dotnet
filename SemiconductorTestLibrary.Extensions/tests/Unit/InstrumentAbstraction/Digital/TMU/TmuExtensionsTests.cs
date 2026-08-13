12dfe4c2b6f1a51480e51a98d19d018d84aa9add
Ashutosh.Mishra <Ashutosh.Mishra@ni.com>
2026-08-13 12:40:37 +0530

    Rename TMU test methods to include initialization

--- SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/Digital/TMU/TmuExtensionsTests.cs
+++ SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/Digital/TMU/TmuExtensionsTests.cs
@@ -908,7 +908,7 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbst
         }
 
         [Fact]
-        public void AssignTMUResourcesOnFirstBundle_AssignSamePinOnSecondBundle_Succeeds()
+        public void InititalizeAndAssignTMUResourcesOnFirstBundle_AssignSamePinOnSecondBundle_Succeeds()
         {
             var sessionManager = InitializeSessionsAndCreateSessionManager();
             var bundle1 = sessionManager.Digital(new string[] { "C0" });
@@ -923,7 +923,7 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Unit.InstrumentAbst
         }
 
         [Fact]
-        public void AssignTMUResourcesOnFirstBundle_ExhaustsResourcesOnSecondBundle_ThrowsAndCleansUp()
+        public void InititalizeAndAssignTMUResourcesOnFirstBundle_ExhaustsResourcesOnSecondBundle_ThrowsAndCleansUp()
         {
             var sessionManager = InitializeSessionsAndCreateSessionManager();
             var bundle1 = sessionManager.Digital(new string[] { "C0", "C1" });
