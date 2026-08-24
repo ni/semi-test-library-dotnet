623603ae36bf16a1153036434f3bc2cf2e4cd64c
Shalini-Subramanian <shalini.subramanian@ni.com>
2026-08-24 11:40:44 +0530

    fix: Remove unused usings
    Signed-off-by: Shalini-Subramanian <shalini.subramanian@ni.com>

--- SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/DCPower/MeasureTests.cs
+++ SemiconductorTestLibrary.Extensions/tests/Unit/InstrumentAbstraction/DCPower/MeasureTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using NationalInstruments.ModularInstruments.NIDCPower;
 using NationalInstruments.SemiconductorTestLibrary.Common;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
