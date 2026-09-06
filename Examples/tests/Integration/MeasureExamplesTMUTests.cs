f069ff6de9a034bbc0a37d44962c760fa774330c
Saxena, Utkarsh <utkarsh.saxena@emerson.com>
2026-09-06 23:56:13 +0530

    Update tests to assert publish

--- Examples/tests/Integration/MeasureExamplesTMUTests.cs
+++ Examples/tests/Integration/MeasureExamplesTMUTests.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using NationalInstruments.Examples.SemiconductorTestLibrary.CodeSnippets.InstrumentAbstraction;
 using NationalInstruments.TestStand.SemiconductorModule.CodeModuleAPI;
+using NationalInstruments.TestStand.SemiconductorModule.Restricted;
 using Xunit;
 using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
 using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
@@ -11,16 +13,29 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
     {
         private const string PinMapFileName = @"NIDigitalTMUTest.pinmap";
         private const string DigitalProjectFileName = @"NIDigitalTMUTest.digiproj";
-        private ISemiconductorModuleContext _tsmContext = CreateTSMContext(PinMapFileName, DigitalProjectFileName);
+        private readonly ISemiconductorModuleContext _tsmContext;
+        private readonly IPublishedDataReader _publishedDataReader;
+
+        public MeasureExamplesTMUTests()
+        {
+            _tsmContext = CreateTSMContext(PinMapFileName, out _publishedDataReader, DigitalProjectFileName);
+        }
 
         #region Measure Duty Cycle TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasureDutyCycleWithSTLSucceeds()
+        public void InitializeNIDigital_MeasureDutyCycleWithSTL_PublishesDutyCycleRatioInRange()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasureDutyCycleTMU.MeasureDutyCycleWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForDutyCycle = publishedData.Where(d => d.PublishedDataId == "DutyCycleRatio").ToArray();
+            Assert.NotEmpty(publishedDataForDutyCycle);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForDutyCycle.Length);
+            Assert.All(publishedDataForDutyCycle, d => Assert.InRange(d.DoubleValue, 0.0, 1.0));
+
             CleanupInstrumentation(_tsmContext);
         }
 
@@ -29,11 +44,17 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
         #region Measure Fall Time TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasureFallTimeWithSTLSucceeds()
+        public void InitializeNIDigital_MeasureFallTimeWithSTL_PublishesFallTimeForEachSite()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasureFallTimeTMU.MeasureFallTimeWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForFallTime = publishedData.Where(d => d.PublishedDataId == "FallTime").ToArray();
+            Assert.NotEmpty(publishedDataForFallTime);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForFallTime.Length);
+
             CleanupInstrumentation(_tsmContext);
         }
 
@@ -42,11 +63,17 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
         #region Measure Period TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasurePeriodWithSTLSucceeds()
+        public void InitializeNIDigital_MeasurePeriodWithSTL_PublishesPeriodForEachSite()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasurePeriodTMU.MeasurePeriodWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForPeriod = publishedData.Where(d => d.PublishedDataId == "Period").ToArray();
+            Assert.NotEmpty(publishedDataForPeriod);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForPeriod.Length);
+
             CleanupInstrumentation(_tsmContext);
         }
 
@@ -55,11 +82,17 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
         #region Measure Pulse Width TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasurePulseWidthWithSTLSucceeds()
+        public void InitializeNIDigital_MeasurePulseWidthWithSTL_PublishesPulseWidthForEachSite()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasurePulseWidthTMU.MeasurePulseWidthWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForPulseWidth = publishedData.Where(d => d.PublishedDataId == "PulseWidth").ToArray();
+            Assert.NotEmpty(publishedDataForPulseWidth);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForPulseWidth.Length);
+
             CleanupInstrumentation(_tsmContext);
         }
 
@@ -68,11 +101,17 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
         #region Measure Rise Time TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasureRiseTimeWithSTLSucceeds()
+        public void InitializeNIDigital_MeasureRiseTimeWithSTL_PublishesRiseTimeForEachSite()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasureRiseTimeTMU.MeasureRiseTimeWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForRiseTime = publishedData.Where(d => d.PublishedDataId == "RiseTime").ToArray();
+            Assert.NotEmpty(publishedDataForRiseTime);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForRiseTime.Length);
+
             CleanupInstrumentation(_tsmContext);
         }
 
@@ -81,11 +120,17 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
         #region Measure Skew TMU Tests
 
         [Fact]
-        public void InitializeNIDigital_MeasureSkewWithSTLSucceeds()
+        public void InitializeNIDigital_MeasureSkewWithSTL_PublishesSkewForEachSite()
         {
             SetupNIDigitalPatternInstrumentation(_tsmContext);
 
             MeasureSkewTMU.MeasureSkewWithSTL(_tsmContext);
+
+            var publishedData = _publishedDataReader.GetAndClearPublishedData();
+            var publishedDataForSkew = publishedData.Where(d => d.PublishedDataId == "Skew").ToArray();
+            Assert.NotEmpty(publishedDataForSkew);
+            Assert.Equal(_tsmContext.SiteNumbers.Count, publishedDataForSkew.Length);
+
             CleanupInstrumentation(_tsmContext);
         }
 
