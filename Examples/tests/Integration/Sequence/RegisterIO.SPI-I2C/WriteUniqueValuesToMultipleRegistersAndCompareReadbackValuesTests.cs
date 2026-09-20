41db48bfee8c1e522063e31193115c2313a8a668
Shalini-Subramanian <shalini.subramanian@ni.com>
2026-09-20 14:37:46 +0530

    fix: Update integration tests
    Signed-off-by: Shalini-Subramanian <shalini.subramanian@ni.com>

--- Examples/tests/Integration/Sequence/RegisterIO.SPI-I2C/WriteUniqueValuesToMultipleRegistersAndCompareReadbackValuesTests.cs
+++ Examples/tests/Integration/Sequence/RegisterIO.SPI-I2C/WriteUniqueValuesToMultipleRegistersAndCompareReadbackValuesTests.cs
@@ -2,7 +2,9 @@ using NationalInstruments.Examples.SemiconductorTestLibrary.RegisterIO.SPIAndI2C
 using NationalInstruments.Examples.SemiconductorTestLibrary.RegisterIO.SPIAndI2C.DutControl;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
 using Xunit;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
 using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.CommonSteps;
 
 namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
 {
@@ -10,18 +12,23 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
     public class WriteUniqueValuesToMultipleRegistersAndCompareReadbackValuesTests
     {
         private const string PinMapFileName = "STLExample.RegisterIO.SPIAndI2C.pinmap";
+        private const string SupportingMaterialsFolderPath = @"\Examples\source\Sequence\RegisterIO.SPI-I2C\Supporting Materials\DigitalProject";
+        private const string DigitalPatternProjectName = "STLExample.RegisterIO.SPIAndI2C.digiproj";
 
         [Theory(Skip = "Requires the shared digital project and a digital pattern instrument.")]
         [InlineData(CommunicationProtocol.SPI)]
         public void WriteUniqueValuesToMultipleRegistersAndCompareReadbackValues_UniqueValues_ReadbackMatches(CommunicationProtocol protocol)
         {
-            var tsmContext = CreateTSMContext(PinMapFileName, out _);
+            var tsmContext = CreateTSMContext(SupportingMaterialsFolderPath, PinMapFileName, DigitalPatternProjectName, out _);
+            SetupNIDigitalPatternInstrumentation(tsmContext);
+            DutPowerUp(tsmContext, new string[] { "VIN" }, new double[] { 3.3 }, new double[] { 0.002 }, 0, false);
+            TestStep.ConfigureDigitalProtocol(protocol, 7, 8, "SPI_write_template", "SPI_read_template", "source_buffer", "capture_buffer", 1, "reg0", "reg1", "reg2", new[] { "CS", "SCK", "SDI", "SDO" });
 
-            // BME280 writable config registers: ctrl_hum (0xF2), ctrl_meas (0xF4), config (0xF5).
             SiteData<bool[]> comparisonResults = TestStep.WriteUniqueValuesToMultipleRegistersAndCompareReadbackValues(
-                tsmContext, protocol, registerAddresses: new uint[] { 0xF2, 0xF4, 0xF5 }, valuesToWrite: new long[] { 0x01, 0x27, 0xA0 });
+                tsmContext, protocol, registerAddresses: new uint[] { 0xF2 }, valuesToWrite: new long[] { 0x01 });
 
             Assert.All(comparisonResults.SiteNumbers, site => Assert.All(comparisonResults.GetValue(site), Assert.True));
+            DutPowerDown(tsmContext, new string[] { "VIN" }, 0, false);
         }
     }
 }
--- Examples/tests/Integration/Sequence/RegisterIO.SPI-I2C/WriteValueToRegisterAndCompareReadbackValueTests.cs
+++ Examples/tests/Integration/Sequence/RegisterIO.SPI-I2C/WriteValueToRegisterAndCompareReadbackValueTests.cs
@@ -2,7 +2,9 @@ using NationalInstruments.Examples.SemiconductorTestLibrary.RegisterIO.SPIAndI2C
 using NationalInstruments.Examples.SemiconductorTestLibrary.RegisterIO.SPIAndI2C.DutControl;
 using NationalInstruments.SemiconductorTestLibrary.DataAbstraction;
 using Xunit;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.SetupAndCleanupSteps;
 using static NationalInstruments.Tests.SemiconductorTestLibrary.Utilities.TSMContext;
+using static NationalInstruments.SemiconductorTestLibrary.TestStandSteps.CommonSteps;
 
 namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
 {
@@ -10,18 +12,23 @@ namespace NationalInstruments.Tests.SemiconductorTestLibrary.Integration
     public class WriteValueToRegisterAndCompareReadbackValueTests
     {
         private const string PinMapFileName = "STLExample.RegisterIO.SPIAndI2C.pinmap";
+        private const string SupportingMaterialsFolderPath = @"\Examples\source\Sequence\RegisterIO.SPI-I2C\Supporting Materials\DigitalProject";
+        private const string DigitalPatternProjectName = "STLExample.RegisterIO.SPIAndI2C.digiproj";
 
         [Theory(Skip = "Requires the shared digital project and a digital pattern instrument.")]
         [InlineData(CommunicationProtocol.SPI)]
         public void WriteValueToRegisterAndCompareReadbackValue_SharedValue_ReadbackMatches(CommunicationProtocol protocol)
         {
-            var tsmContext = CreateTSMContext(PinMapFileName, out _);
+            var tsmContext = CreateTSMContext(SupportingMaterialsFolderPath, PinMapFileName, DigitalPatternProjectName, out _);
+            SetupNIDigitalPatternInstrumentation(tsmContext);
+            DutPowerUp(tsmContext, new string[] { "VIN" }, new double[] { 3.3 }, new double[] { 0.002 }, 0, false);
+            TestStep.ConfigureDigitalProtocol(protocol, 7, 8, "SPI_write_template", "SPI_read_template", "source_buffer", "capture_buffer", 1, "reg0", "reg1", "reg2", new[] { "CS", "SCK", "SDI", "SDO" });
 
-            // BME280 ctrl_meas (0xF4) is writable and reads back the written value in sleep mode.
             SiteData<bool> comparisonResults = TestStep.WriteValueToRegisterAndCompareReadbackValue(
                 tsmContext, protocol, registerAddress: 0xF4, valueToWrite: 0x27);
 
             Assert.All(comparisonResults.SiteNumbers, site => Assert.True(comparisonResults.GetValue(site)));
+            DutPowerDown(tsmContext, new string[] { "VIN" }, 0, false);
         }
     }
 }
